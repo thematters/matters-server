fc47b7c2367a02f328699b824490a478c9ff515c
Guo Liu <liuguomailbox@gmail.com>
2018-11-29 14:06:07 -0800

    update comment types

--- src/Comment/types.ts
+++ src/Comment/types.ts
@@ -2,20 +2,38 @@ export const types = /* GraphQL */ `
   # extend type Query {
   # }
 
-  # extend type Mutation {
-  # }
+  extend type Mutation {
+    createOrEditComment(comment: CommentInput, commentId: String): Comment
+  }
 
   type Comment {
     id: String!
-    # Creation time of this comment
-    timestamp: DateTime!
-    text: String!
-    achieved: Boolean!
     # Original article of this comment
     article: Article!
+    # content
+    text: String
+    # Creation time of this comment
+    createdAt: DateTime!
     author: User!
+    achieved: Boolean!
+    upvotes: Int!
+    downvotes: Int!
+    myVote: Vote
     mentions: [User]
     comments: [Comment]
     parentComment: Comment
+  }
+
+  input CommentInput {
+    text: String!
+    quote: Boolean!
+    articleId: String!
+    parentId: String
+    mentions: [String]
+  }
+
+  enum Vote {
+    up
+    down
   }
 `
