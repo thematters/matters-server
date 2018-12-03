48d385cc2fb6e4192af33e7eacbe951f7f4fbfd6
Guo Liu <liuguomailbox@gmail.com>
2018-12-03 15:26:50 -0800

    use mock

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,24 @@ const context = (): Context => ({
   actionService: new ActionService()
 })
 
+const mocks = {
+  JSON: () => ({
+    'index.html': '<html><p>hello</p></html>',
+    '1.png': 'some png string'
+  }),
+  DateTime: () => new Date().toISOString(),
+  Email: () => 'test@matters.news',
+  UUID: () => '00000000-0000-0000-0000-000000000001',
+  URL: () => 'test-url'
+}
+
 const server = new ApolloServer({
   schema,
   context,
   engine: {
     apiKey: process.env['ENGINE_API_KEY']
-  }
+  },
+  mocks
 })
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
