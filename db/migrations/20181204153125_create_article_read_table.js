bc7c9284b8c5151471342a4d99c0563a4d774690
Guo Liu <liuguomailbox@gmail.com>
2018-12-04 15:43:26 -0800

    add search history, article read and report article table

---
+++ db/migrations/20181204153125_create_article_read_table.js
@@ -0,0 +1,22 @@
+const table = 'article_read'
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable(table, function(t) {
+    t.bigIncrements('id').primary()
+    t.bigInteger('user_id').notNullable()
+    t.bigInteger('article_id').notNullable()
+    t.timestamp('created_at').defaultTo(knex.fn.now())
+
+    // Setup foreign key
+    t.foreign('user_id')
+      .references('id')
+      .inTable('user')
+    t.foreign('article_id')
+      .references('id')
+      .inTable('article')
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable(table)
+}
