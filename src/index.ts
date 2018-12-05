6a7f52ec0b746800b537ac495e9593ecc71b0c52
Guo Liu <liuguomailbox@gmail.com>
2018-12-04 21:22:17 -0800

    revert mock

--- src/index.ts
+++ src/index.ts
@@ -51,8 +51,8 @@ const server = new ApolloServer({
   context,
   engine: {
     apiKey: process.env['ENGINE_API_KEY']
-  },
-  mocks
+  }
+  // mocks
 })
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
