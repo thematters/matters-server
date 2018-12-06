1ece7a200ecf79f5e3d815ec4233c4ec704d6453
carolusian <pkufashuo400@gmail.com>
2018-12-06 23:56:34 +0800

    Add test db configuration

--- db/testSetup.js
+++ db/testSetup.js
@@ -1,6 +1,11 @@
+// set the global environment to 'test'
+process.env['MATTERS_ENV'] = 'test'
+
+const { Client } = require('pg')
 const Knex = require('knex')
 const knexConfig = require('../knexfile')
 const knex = Knex(knexConfig[process.env['MATTERS_ENV']])
+const database = knexConfig[process.env['MATTERS_ENV']].connection.database
 
 global.knex = knex
 
@@ -14,6 +19,22 @@ module.exports = async () => {
       return
     }
   }
+
+  const client = new Client({
+    host: process.env['MATTERS_PG_HOST'],
+    user: process.env['MATTERS_PG_USER'],
+    password: process.env['MATTERS_PG_PASSWORD'],
+    database: 'postgres',
+  })
+
+  // create test db if it does not exist
+  client.connect()
+  const result = await client.query('SELECT * FROM pg_catalog.pg_database WHERE datname = \'' + database + '\'')
+  if (!result.rowCount) {
+    await client.query('CREATE DATABASE "' + database + '"')
+  }
+  client.end()
+
   await rollbackAllMigrations()
   await knex.migrate.latest()
   await knex.seed.run()
