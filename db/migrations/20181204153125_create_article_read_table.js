77098222ae9eb3365c054a1326120e819024e853
Zeck Li <>
2019-01-07 13:01:12 +0800

    Revise article read schema

--- db/migrations/20181204153125_create_article_read_table.js
+++ db/migrations/20181204153125_create_article_read_table.js
@@ -11,7 +11,6 @@ exports.up = async knex => {
       .unique()
     t.bigInteger('user_id')
       .unsigned()
-      .notNullable()
     t.bigInteger('article_id')
       .unsigned()
       .notNullable()
@@ -19,6 +18,9 @@ exports.up = async knex => {
     t.timestamp('created_at').defaultTo(knex.fn.now())
 
     // Setup foreign key
+    t.foreign('user_id')
+      .references('id')
+      .inTable('user')
     t.foreign('article_id')
       .references('id')
       .inTable('article')
