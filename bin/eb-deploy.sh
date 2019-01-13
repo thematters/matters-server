8b406e8f590609854a6e8943192789c65fd5458c
carolusian <pkufashuo400@gmail.com>
2019-01-13 15:03:22 +0800

    Add

--- bin/eb-deploy.sh
+++ bin/eb-deploy.sh
@@ -2,29 +2,31 @@
 TEMPLATE="docker/Dockerrun.aws.json.example"
 OUT_CONFIG="Dockerrun.aws.json"
 
-APP_PRODUCTION="matters-production"
-ENV_PRODUCTION="matters-server-production"
+APP_PRODUCTION="matters-prod"
+ENV_PRODUCTION="matters-server-prod"
 REGION_PRODUCTION="ap-southeast-1"
-IMAGE_TAG_PRODUCTION="production"
+IMAGE_TAG_PRODUCTION="prod"
 
 APP_STAGING="matters-stage"
 ENV_STAGING="matters-server-stage"
 REGION_STAGING="ap-southeast-1"
 IMAGE_TAG_STAGING="staging"
 
-if [[ $1 == 'production' ]]
+if [[ $1 == 'prod' ]]
 then
     echo "Deploying to production environment..."
     cat $TEMPLATE | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG_PRODUCTION/" > $OUT_CONFIG
     printf 'n' | eb init $APP_PRODUCTION --region $REGION_PRODUCTION
-    eb deploy $ENV_PRODUCTION
+    eb use $ENV_PRODUCTION
+    eb deploy
 elif [[ $1 == 'staging' ]]
 then
     echo "Deploying to staging environment..."
     cat $TEMPLATE | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG_STAGING/" > $OUT_CONFIG
     printf 'n' | eb init $APP_STAGING --region $REGION_STAGING
-    eb deploy $ENV_STAGING
+    eb use $ENV_STAGING
+    eb deploy
 else
-    echo "Usage: bin/eb-deploy.sh [production|staging]"
+    echo "Usage: bin/eb-deploy.sh [prod|staging]"
 fi
 
