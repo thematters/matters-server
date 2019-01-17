d889956a56f68b1f3ad4a9bfba005ea9b3088e00
carolusian <pkufashuo400@gmail.com>
2019-01-17 18:33:37 +0800

    Update CI to deploy to develop|staging|prod environments

--- bin/eb-deploy.sh
+++ bin/eb-deploy.sh
@@ -2,29 +2,40 @@
 TEMPLATE="docker/Dockerrun.aws.json.example"
 OUT_CONFIG="Dockerrun.aws.json"
 
-APP_PRODUCTION="matters-prod"
-ENV_PRODUCTION="matters-server-prod"
-REGION_PRODUCTION="ap-southeast-1"
-IMAGE_TAG_PRODUCTION="prod"
+APP_DEVELOP="matters-develop"
+ENV_DEVELOP="matters-server-develop"
+REGION_DEVELOP="ap-southeast-1"
+IMAGE_TAG_DEVELOP="develop"
 
 APP_STAGING="matters-stage"
 ENV_STAGING="matters-server-stage"
 REGION_STAGING="ap-southeast-1"
 IMAGE_TAG_STAGING="staging"
 
-if [[ $1 == 'prod' ]]
+APP_PRODUCTION="matters-prod"
+ENV_PRODUCTION="matters-server-prod"
+REGION_PRODUCTION="ap-southeast-1"
+IMAGE_TAG_PRODUCTION="prod"
+
+if [[ $1 == 'develop' ]]
 then
-    echo "Deploying to production environment..."
-    cat $TEMPLATE | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG_PRODUCTION/" > $OUT_CONFIG
-    printf '\n\n' | eb init $APP_PRODUCTION --region $REGION_PRODUCTION
-    eb deploy $ENV_PRODUCTION
+    echo "Deploying to development environment..."
+    cat $TEMPLATE | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG_DEVELOP/" > $OUT_CONFIG
+    printf '\n\n' | eb init $APP_DEVELOP --region $REGION_DEVELOP
+    eb deploy $ENV_DEVELOP
 elif [[ $1 == 'staging' ]]
 then
     echo "Deploying to staging environment..."
     cat $TEMPLATE | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG_STAGING/" > $OUT_CONFIG
     printf '\n\n' | eb init $APP_STAGING --region $REGION_STAGING
     eb deploy $ENV_STAGING
+elif [[ $1 == 'prod' ]]
+then
+    echo "Deploying to production environment..."
+    cat $TEMPLATE | sed "s/{{IMAGE_TAG}}/$IMAGE_TAG_PRODUCTION/" > $OUT_CONFIG
+    printf '\n\n' | eb init $APP_PRODUCTION --region $REGION_PRODUCTION
+    eb deploy $ENV_PRODUCTION
 else
-    echo "Usage: bin/eb-deploy.sh [prod|staging]"
+    echo "Usage: bin/eb-deploy.sh [develop|staging|prod]"
 fi
 
