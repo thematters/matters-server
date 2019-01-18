0e5c2254563c041185f04ebca22ca061c8188b6d
Guo Liu <liuguomailbox@gmail.com>
2019-01-17 23:05:54 -0800

    put back uuid

--- db/migrations/20190118013306_add_ip_to_article_read.js
+++ db/migrations/20190118013306_add_ip_to_article_read.js
@@ -3,13 +3,11 @@ const table = 'article_read'
 exports.up = async knex => {
   await knex.schema.table(table, function(t) {
     t.string('ip')
-    t.dropColumn('uuid')
   })
 }
 
 exports.down = async knex => {
   await knex.schema.table(table, function(t) {
     t.dropColumn('ip')
-    t.uuid('uuid')
   })
 }
