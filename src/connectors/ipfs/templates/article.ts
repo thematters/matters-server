0cdaf4304cb28a4baf1b533fc7f4828b16461657
RoberTu <robertu0717@gmail.com>
2019-01-24 11:53:28 +0800

    Improve spacing

--- src/connectors/ipfs/templates/article.ts
+++ src/connectors/ipfs/templates/article.ts
@@ -58,6 +58,9 @@ const style =
     color: #5F5F5F;
   }
 
+  header {
+    margin-bottom: 40px;
+  }
   header h1 {
     font-size: 32px;
   }
@@ -78,8 +81,8 @@ const style =
   }
 
   article > * {
-    margin-top: 32px;
-    margin-bottom: 32px;
+    margin-top: 20px;
+    margin-bottom: 24px;
   }
   article a {
     border-bottom: 1px solid currentcolor;
@@ -87,7 +90,7 @@ const style =
     padding-bottom: 2px;
   }
   article p {
-    line-height: 2;
+    line-height: 1.8;
   }
   figure.summary {
 
