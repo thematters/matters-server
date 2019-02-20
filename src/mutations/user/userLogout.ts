d2663a726041b23a6f7a072d9857ee5948871a64
Guo Liu <liuguomailbox@gmail.com>
2019-02-20 14:19:21 -0800

    add logout mutation

--- src/mutations/user/userLogout.ts
+++ src/mutations/user/userLogout.ts
@@ -1,22 +1,8 @@
-// import { MutationToUserLoginResolver } from 'definitions'
+import { MutationToUserLogoutResolver } from 'definitions'
 
-// const resolver: MutationToUserLoginResolver = async (
-//   root,
-//   { input },
-//   { dataSources: { userService }, res }
-// ) => {
-//   try {
-//     const { token, auth, expiresIn } = await userService.login({
-//       ...input,
-//       email: input.email ? input.email.toLowerCase() : null
-//     })
+const resolver: MutationToUserLogoutResolver = async (root, args, { res }) => {
+  res.clearCookie('token')
+  return true
+}
 
-//     setCookie({ res, token, expiresIn })
-
-//     return { token, auth }
-//   } catch (err) {
-//     throw err
-//   }
-// }
-
-// export default resolver
+export default resolver
