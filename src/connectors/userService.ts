ae9e55c1aa255d66f9b4d8f674f0086853e77fba
Guo Liu <liuguomailbox@gmail.com>
2019-03-19 17:01:12 -0700

    add secondary orders

--- src/connectors/userService.ts
+++ src/connectors/userService.ts
@@ -479,6 +479,7 @@ export class UserService extends BaseService {
     const result = await this.knex(table)
       .select()
       .orderByRaw('author_score DESC NULLS LAST')
+      .orderBy('id', 'desc')
       .offset(offset)
       .limit(limit)
       .whereNotIn('id', notIn)
