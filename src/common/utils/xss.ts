4ca55468b77ed62e80b9179463b86c65618ca254
RoberTu <robertu0717@gmail.com>
2019-03-27 21:01:14 +0800

    Allow attributes of whitelist tags start with "data-"

--- src/common/utils/xss.ts
+++ src/common/utils/xss.ts
@@ -1,17 +1,24 @@
 import xss from 'xss'
 
 const CUSTOM_WHITE_LISTS = {
-  a: [
-    ...(xss.whiteList.a || []),
-    'class',
-    'data-id',
-    'data-user-name',
-    'data-display-name'
-  ],
+  a: [...(xss.whiteList.a || []), 'class'],
   figure: [],
   figcaption: [],
   iframe: ['src', 'class']
 }
 
 export const sanitize = (string: string) =>
-  xss(string, { whiteList: { ...xss.whiteList, ...CUSTOM_WHITE_LISTS } })
+  xss(string, {
+    whiteList: { ...xss.whiteList, ...CUSTOM_WHITE_LISTS },
+    onIgnoreTagAttr: function(tag, name, value, isWhiteAttr) {
+      /**
+       * Allow attributes of whitelist tags start with "data-"
+       *
+       * @see https://github.com/leizongmin/js-xss#allow-attributes-of-whitelist-tags-start-with-data-
+       */
+      if (name.substr(0, 5) === 'data-') {
+        // escape its value using built-in escapeAttrValue function
+        return name + '="' + xss.escapeAttrValue(value) + '"'
+      }
+    }
+  })
