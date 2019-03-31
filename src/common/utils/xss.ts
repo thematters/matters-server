af8d35ad28fd1ff50639e899e12d6fd41e7adae7
RoberTu <robertu0717@gmail.com>
2019-03-31 18:09:17 +0800

    Allow "class" attribute for all tags; Add "frameborder", "allowfullscreen", "sandbox" attributes for <iframe>

--- src/common/utils/xss.ts
+++ src/common/utils/xss.ts
@@ -1,19 +1,18 @@
 import xss from 'xss'
 
 const CUSTOM_WHITE_LISTS = {
-  a: [...(xss.whiteList.a || []), 'class'],
   figure: [],
   figcaption: [],
-  iframe: ['src', 'class', 'style']
+  iframe: ['src', 'frameborder', 'allowfullscreen', 'sandbox']
 }
 
 const onIgnoreTagAttr = (tag: string, name: string, value: string) => {
   /**
-   * Allow attributes of whitelist tags start with "data-"
+   * Allow attributes of whitelist tags start with "data-" or "class"
    *
    * @see https://github.com/leizongmin/js-xss#allow-attributes-of-whitelist-tags-start-with-data-
    */
-  if (name.substr(0, 5) === 'data-') {
+  if (name.substr(0, 5) === 'data-' || name.substr(0, 5) === 'class') {
     // escape its value using built-in escapeAttrValue function
     return name + '="' + xss.escapeAttrValue(value) + '"'
   }
