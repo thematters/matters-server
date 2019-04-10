707514a26c2c2f9045c20566ffac183fd14b37da
Zeck Li <devformatters@gmail.com>
2019-04-10 14:39:23 +0800

    Replace js code with sql string

--- db/migrations/20190408163001_gen_data_for_collection.js
+++ db/migrations/20190408163001_gen_data_for_collection.js
@@ -1,5 +1,4 @@
 exports.up = async knex => {
-
   await knex.raw(/*sql*/ `
     INSERT INTO collection (entrance_id, article_id, "order")
     SELECT id, upstream_id, 0 FROM article WHERE upstream_id IS NOT NULL
