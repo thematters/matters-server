964bcdca1407b3d61dd6ff022bd1835d2c9614b8
Zeck Li <devformatters@gmail.com>
2019-04-10 14:38:42 +0800

    Replace js code with sql string

--- db/migrations/20190408163001_gen_data_for_collection.js
+++ db/migrations/20190408163001_gen_data_for_collection.js
@@ -1,25 +1,9 @@
 exports.up = async knex => {
-  // Gather ids
-  const data = await knex('article')
-    .select('upstream_id')
-    .whereNotNull('upstream_id')
-    .groupBy('upstream_id')
-    .orderBy('upstream_id')
 
-  // Produce and insert collection data
-  for (const item of data) {
-    const articles = await knex('article')
-      .where(item)
-      .orderBy('created_at')
-
-    const collections = articles.map((article, index) => ({
-      entrance_id: item.upstream_id,
-      article_id: article.id,
-      order: index + 1
-    }))
-
-    const insertions = await knex.batchInsert('collection', collections, 30)
-  }
+  await knex.raw(/*sql*/ `
+    INSERT INTO collection (entrance_id, article_id, "order")
+    SELECT id, upstream_id, 0 FROM article WHERE upstream_id IS NOT NULL
+  `)
 }
 
 exports.down = async knex => {
