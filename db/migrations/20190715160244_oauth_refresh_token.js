bbc2c0432310a877167dec67ea05ef3ab460a09c
RoberTu <robertu0717@gmail.com>
2019-07-15 16:56:09 +0800

    Add	OAuth tables

---
+++ db/migrations/20190715160244_oauth_refresh_token.js
@@ -0,0 +1,38 @@
+const {
+  baseDown
+} = require('../utils')
+
+const table = 'oauth_refresh_token'
+
+exports.up = async knex => {
+  await knex('entity_type').insert({
+    table
+  })
+  await knex.schema.createTable(table, t => {
+    t.bigIncrements('id').primary()
+    t.timestamp('created_at').defaultTo(knex.fn.now())
+    t.timestamp('updated_at').defaultTo(knex.fn.now())
+
+    t.string('token')
+      .notNullable()
+      .unique()
+    t.bigInteger('client_id')
+      .unsigned()
+      .notNullable()
+    t.bigInteger('user_id')
+      .unsigned()
+      .notNullable()
+    t.timestamp('expires')
+    t.text('scope')
+
+    // Setup foreign key
+    t.foreign('client_id')
+      .references('id')
+      .inTable('oauth_client')
+    t.foreign('user_id')
+      .references('id')
+      .inTable('user')
+  })
+}
+
+exports.down = baseDown(table)
