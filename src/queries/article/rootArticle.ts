692bcb0e730dc26d9325a8315d1742ab17fa5e1e
Zeck Li <devformatters@gmail.com>
2019-07-23 15:07:59 +0800

    Add temporarily checker for block users.

--- src/queries/article/rootArticle.ts
+++ src/queries/article/rootArticle.ts
@@ -1,21 +1,31 @@
 import { QueryToArticleResolver } from 'definitions'
 
+import { BLOCK_USERS } from 'common/enums'
+
 const resolver: QueryToArticleResolver = async (
   root,
   { input: { mediaHash, uuid } },
-  { viewer, dataSources: { articleService } }
+  { viewer, dataSources: { articleService, userService } }
 ) => {
   if (!mediaHash && !uuid) {
     return
   }
 
+  let article
   if (mediaHash) {
-    return articleService.findByMediaHash(mediaHash)
+    article = await articleService.findByMediaHash(mediaHash)
+  }
+  else if (uuid) {
+    article = await articleService.baseFindByUUID(uuid)
   }
 
-  if (uuid) {
-    return articleService.baseFindByUUID(uuid)
+  if (article) {
+    const user = await userService.dataloader.load(article.authorId)
+    if (user && BLOCK_USERS.includes(user.userName)) {
+      return
+    }
   }
+  return article
 }
 
 export default resolver
--- src/queries/user/rootUser.ts
+++ src/queries/user/rootUser.ts
@@ -1,13 +1,16 @@
 import { QueryToUserResolver } from 'definitions'
 
+import { BLOCK_USERS } from 'common/enums'
+
 const resolver: QueryToUserResolver = async (
   root,
   { input: { userName } },
   { viewer, dataSources: { userService } }
 ) => {
-  if (!userName) {
+  if (!userName || BLOCK_USERS.includes(userName)) {
     return
   }
+
   return userService.findByUserName(userName)
 }
 
