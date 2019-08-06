e82a07a571dc6b0759eb36dcd2fb14d89459022f
Zeck Li <devformatters@gmail.com>
2019-08-06 16:26:59 +0800

    Add migration script for adding sticky field.

---
+++ db/migrations/20190806010311_add_article_sticky.js
@@ -0,0 +1,13 @@
+const table = 'article'
+
+exports.up = async knex => {
+  await knex.schema.table(table, function(t) {
+    t.boolean('sticky').defaultTo(false)
+  })
+}
+
+exports.down = async knex => {
+  await knex.schema.table(table, function(t) {
+    t.dropColumn('sticky')
+  })
+}
