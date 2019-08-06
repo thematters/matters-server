3eea4475d9a5de84a6e95f52594317b7aabc7aaa
RoberTu <robertu0717@gmail.com>
2019-08-06 18:49:52 +0800

    formatting

--- db/migrations/20190806175221_oauth_client_text_array.js
+++ db/migrations/20190806175221_oauth_client_text_array.js
@@ -1,7 +1,7 @@
 const table = 'oauth_client'
 
 exports.up = async knex => {
-  await knex.schema.table(table, function (t) {
+  await knex.schema.table(table, function(t) {
     t.specificType('redirect_uri', 'text ARRAY').alter()
     t.specificType('grant_types', 'text ARRAY').alter()
     t.specificType('scope', 'text ARRAY').alter()
@@ -9,7 +9,7 @@ exports.up = async knex => {
 }
 
 exports.down = async knex => {
-  await knex.schema.table(table, function (t) {
+  await knex.schema.table(table, function(t) {
     t.text('scope').alter()
     t.string('grant_types').alter()
     t.text('redirect_uri').alter()
