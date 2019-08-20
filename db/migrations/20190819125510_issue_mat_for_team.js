ff2f9d62d8d805ef3431366e5bea2166556c6f21
Zeck Li <devformatters@gmail.com>
2019-08-20 11:59:59 +0800

    Add migration script for issuing MAT.

---
+++ db/migrations/20190819125510_issue_mat_for_team.js
@@ -0,0 +1,80 @@
+const compact = require('lodash/compact')
+
+const uuidv4 = require('uuid/v4')
+
+const table = 'transaction'
+
+const purpose = 'system-subsidy'
+
+exports.up = async knex => {
+  const items = [
+    {
+      userName: 'lalaland',
+      role: 'admin',
+      amount: 2000
+    },
+    {
+      userName: 'yingshinlee',
+      role: 'admin',
+      amount: 2000
+    },
+    {
+      userName: 'satsuki',
+      role: 'admin',
+      amount: 2000
+    },
+    {
+      userName: 'robertu',
+      role: 'user',
+      amount: 2000
+    },
+    {
+      userName: 'guo',
+      role: 'admin',
+      amount: 2000
+    },
+    {
+      userName: 'baize417',
+      role: 'admin',
+      amount: 1000
+    },
+    {
+      userName: 'andyischaos',
+      role: 'admin',
+      amount: 500
+    }
+  ]
+
+  const users = compact(
+    await Promise.all(
+      items.map(async ({ userName, role, amount }) => {
+        const user = await knex('user')
+          .select('id')
+          .where({ user_name: userName, role })
+          .first()
+        if (user) {
+          return { id: user.id, amount }
+        }
+      })
+    )
+  )
+
+  const result = await Promise.all(
+    users.map(({ id, amount }) =>
+      knex(table)
+        .insert({
+          uuid: uuidv4(),
+          recipient_id: id,
+          reference_id: id,
+          purpose,
+          amount
+        })
+        .into(table)
+        .returning(['id', 'recipient_id', 'amount'])
+    )
+  )
+
+  console.log(result)
+}
+
+exports.down = () => {}
