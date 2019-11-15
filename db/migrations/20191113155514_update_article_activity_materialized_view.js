a13c0fd3ea8fc124fa1286f198d30467341680c5
Guo Liu <liuguomailbox@gmail.com>
2019-11-14 16:53:12 -0800

    update hottest rank

--- db/migrations/20191113155514_update_article_activity_materialized_view.js
+++ db/migrations/20191113155514_update_article_activity_materialized_view.js
@@ -5,64 +5,64 @@ exports.up = async knex => {
 
   create view article_activity_view as
   select id,
-         title,
-         created_at,
-         base_score,
-         boost_score_1,
-         boost_score_2,
-         base_score+boost_score_1+boost_score_2 as score
+    title,
+    created_at,
+    base_score,
+    boost_score_1,
+    boost_score_2,
+    base_score+boost_score_1+boost_score_2 as score
   from
-    (select article.id,
-            article.title,
-            article.created_at,
-            (0.5*coalesce(comment_3_day, 0)+2*coalesce(like_5_day, 0)) * (2+post_days)/(1+post_days) as base_score,
-            (15*coalesce(comment_30_min, 0) + 80*coalesce(like_30_minute, 0)) as boost_score_1,
-            greatest(120-2*since_comment, 0) + greatest(120-2*since_like, 0) as boost_score_2
-     from article /* past comment count */
-     left join
-       (select article_id,
-               sum((created_at >= now() - interval '1 days')::int) as comment_3_day,
-               sum((created_at >= now() - interval '30 minutes')::int) as comment_30_min
-        from comment
-        group by article_id) as cc on cc.article_id = article.id /* past 5 days like */
-     left join
-       (select reference_id,
-               sum(amount) as like_5_day
-        from transaction
-        where purpose = 'appreciate'
-          and created_at >= now() - interval '2 days'
-        group by reference_id) as lc1 on lc1.reference_id = article.id /* past 30 minutes like */
-     left join
-       (select reference_id,
-               sum(amount) as like_30_minute
-        from transaction
-        where purpose = 'appreciate'
-          and created_at >= now() - interval '30 minutes'
-        group by reference_id) as lc2 on lc2.reference_id = article.id /* number of days since published */
-     left join
-       (select id,
-               current_date - created_at::date as post_days
-        from article) as pd on pd.id = article.id /* minutes since first comment */
-     left join
-       (select article_id,
-               extract(epoch
-                       from now()-min(created_at))/60 as since_comment
-        from comment
-        where created_at >= now() - interval '30 days'
-        group by article_id) as fc on fc.article_id = article.id /* minutes since first like */
-     left join
-       (select reference_id,
-               extract(epoch
-                       from now()-min(created_at))/60 as since_like
-        from transaction
-        where purpose = 'appreciate'
-          and created_at >= now() - interval '30 days'
-        group by reference_id) as fl on fl.reference_id = article.id) as scores
+   (select article.id,
+      article.title,
+      article.created_at,
+      (0.5*coalesce(comment_9_hrs, 0)+2*coalesce(like_18_hrs, 0)) * (2+post_days)/(1+post_days) as base_score,
+      (15*coalesce(comment_30_mins, 0) + 60*coalesce(like_30_mins, 0)) as boost_score_1,
+      greatest(120-2*since_comment, 0) + greatest(240-2*since_like, 0) as boost_score_2
+  from article /* past comment count */
+  left join
+   (select article_id,
+          sum((created_at >= now() - interval '9 hours')::int) as comment_9_hrs,
+          sum((created_at >= now() - interval '30 minutes')::int) as comment_30_mins
+  from comment
+  group by article_id) as cc on cc.article_id = article.id /* past 2 days like */
+  left join
+   (select reference_id,
+          sum(amount) as like_18_hrs
+  from transaction
+  where purpose = 'appreciate'
+    and created_at >= now() - interval '18 hours'
+  group by reference_id) as lc1 on lc1.reference_id = article.id /* past 30 minutes like */
+  left join
+   (select reference_id,
+          sum(amount) as like_30_mins
+  from transaction
+  where purpose = 'appreciate'
+    and created_at >= now() - interval '30 minutes'
+  group by reference_id) as lc2 on lc2.reference_id = article.id /* number of days since published */
+  left join
+   (select id,
+          current_date - created_at::date as post_days
+  from article) as pd on pd.id = article.id /* minutes since first comment */
+  left join
+   (select article_id,
+          extract(epoch
+                  from now()-min(created_at))/60 as since_comment
+  from comment
+  where created_at >= now() - interval '30 days'
+  group by article_id) as fc on fc.article_id = article.id /* minutes since first like */
+  left join
+   (select reference_id,
+          extract(epoch
+                  from now()-min(created_at))/60 as since_like
+  from transaction
+  where purpose = 'appreciate'
+    and created_at >= now() - interval '30 days'
+  group by reference_id) as fl on fl.reference_id = article.id) as scores
   where id not in
-      (select id
-       from matters_today
-       order by updated_at desc
-       limit 1);
+   (select id
+    from matters_today
+    order by updated_at desc
+    limit 1);
 
 
   create materialized view article_activity_materialized as
