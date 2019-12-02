79ef1a984ad9ff58ed36cecf7d9ffb10d10087e8
Zeck Li <devformatters@gmail.com>
2019-12-02 11:19:48 +0800

    Alter user notify setting for tag.

---
+++ db/migrations/20191202020522_add_tag_notify_setting.js
@@ -0,0 +1,15 @@
+const table = 'user_notify_setting'
+
+exports.up = async knex => {
+  await knex.schema.table(table, t => {
+    t.boolean('tag')
+      .notNullable()
+      .defaultTo(true)
+  })
+}
+
+exports.down = async knex => {
+  await knex.schema.table(table, t => {
+    t.dropColumn('tag')
+  })
+}
