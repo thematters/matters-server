742d88b384f4c817ef433449a0aabbcfa9d5c21b
RoberTu <robertu0717@gmail.com>
2019-12-06 14:38:40 +0800

    send user deleted email in `updateUserState` mutation

--- src/mutations/user/updateUserState.ts
+++ src/mutations/user/updateUserState.ts
@@ -9,9 +9,10 @@ const resolver: MutationToUpdateUserStateResolver = async (
   { viewer, dataSources: { userService, notificationService } }
 ) => {
   const { id: dbId } = fromGlobalId(id)
+  const isArchived = state === USER_STATE.archived
 
   // verify password if target state is `archived`
-  if (state === USER_STATE.archived) {
+  if (isArchived) {
     if (!password || !viewer.id) {
       throw new UserInputError('`password` is required for archiving user')
     } else {
@@ -33,6 +34,17 @@ const resolver: MutationToUpdateUserStateResolver = async (
     notificationService.trigger({
       event: 'user_frozen',
       recipientId: user.id
+    })
+  }
+
+  // send user deleted email
+  if (isArchived) {
+    notificationService.mail.sendUserDeletedByAdmin({
+      to: user.email,
+      recipient: {
+        displayName: user.displayName
+      },
+      language: user.language
     })
   }
 
