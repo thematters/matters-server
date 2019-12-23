70c0078ff4d27772d8b743a0b0fcc7c27168308e
RoberTu <robertu0717@gmail.com>
2019-12-23 21:25:26 +0800

    formatting

--- db/migrations/20191222174425_update_user_state.js
+++ db/migrations/20191222174425_update_user_state.js
@@ -1,7 +1,10 @@
 const table = 'user'
 
-exports.up = async knex => await knex(table).where({
-  state: 'onboarding'
-}).update('state', 'active')
+exports.up = async knex =>
+  await knex(table)
+    .where({
+      state: 'onboarding'
+    })
+    .update('state', 'active')
 
 exports.down = () => {}
