2161ed42c41662d882d63e71454c837c2ebbb1dc
Zeck Li <devformatters@gmail.com>
2020-01-09 16:15:58 +0800

    Alter user_oauth table columns.

---
+++ db/migrations/20200109153100_alter_user_oauth_scope.js
@@ -0,0 +1,17 @@
+const table = 'user_oauth'
+
+exports.up = async knex => {
+  await knex.schema.table(table, function(t) {
+    t.string('refresh_token').nullable().alter()
+    t.text('scope')
+    t.timestamp('expires')
+  })
+}
+
+exports.down = async knex => {
+  await knex.schema.table(table, function(t) {
+    t.string('refresh_token').notNullable().alter()
+    t.dropColumn('scope')
+    t.dropColumn('expires')
+  })
+}
