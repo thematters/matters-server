5d8ac3ea7172915f48804342ae7eaaa3c65d679e
Zeck Li <devformatters@gmail.com>
2020-02-25 11:13:16 +0800

    Replace insert with batch insert for article_read migration.

--- db/migrations/20200220061530_migrate_article_read.js
+++ db/migrations/20200220061530_migrate_article_read.js
@@ -4,7 +4,7 @@ const uuid = require('uuid')
 const source = 'article_read'
 const target = 'article_read_count'
 
-const size = 100
+const size = 300
 
 exports.up = async knex => {
   const querySource = knex(source)
