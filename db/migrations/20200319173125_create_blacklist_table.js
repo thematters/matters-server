2558481ee61490deeb63c60ea0c3755058c4b7ad
Zeck Li <devformatters@gmail.com>
2020-03-20 11:34:53 +0800

    Add blacklist table.

---
+++ db/migrations/20200319173125_create_blacklist_table.js
@@ -0,0 +1,18 @@
+const { baseDown } = require('../utils')
+
+const table = 'blacklist'
+
+exports.up = async knex => {
+  await knex('entity_type').insert({ table })
+  await knex.schema.createTable(table, t => {
+    t.bigIncrements('id').primary()
+    t.uuid('uuid').notNullable()
+    t.enu('type', ['agent_hash', 'email']).notNullable()
+    t.string('value').notNullable()
+    t.boolean('archived').defaultTo(false)
+    t.timestamp('created_at').defaultTo(knex.fn.now())
+    t.timestamp('updated_at').defaultTo(knex.fn.now())
+  })
+}
+
+exports.down = baseDown(table)
