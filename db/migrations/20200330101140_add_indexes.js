40fcd4ca00bfcf65461b8ccafa08f2469789c72f
robertu <robertu0717@gmail.com>
2020-03-30 11:21:24 +0800

    Update comment index

--- db/migrations/20200330101140_add_indexes.js
+++ db/migrations/20200330101140_add_indexes.js
@@ -4,13 +4,13 @@ const table_comment = 'comment'
 exports.up = async knex => {
   // comment
   await knex.schema.table(table_comment, t => {
-    t.index('author_id').index('state')
+    t.index(['author_id', 'state'])
   })
 }
 
 exports.down = async knex => {
   // comment
   await knex.schema.table(table_comment, t => {
-    t.dropIndex('author_id').dropIndex('state')
+    t.dropIndex(['author_id', 'state'])
   })
 }
