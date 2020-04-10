b14955f0c6d5af8d5e42d0dac2713386396bfec8
robertu <robertu0717@gmail.com>
2020-04-10 16:43:46 +0800

    fix typo

--- db/migrations/20200410163703_add_appreciation_indexes.js
+++ db/migrations/20200410163703_add_appreciation_indexes.js
@@ -1,13 +1,13 @@
-const transaction = 'appreciation'
+const table = 'appreciation'
 
 exports.up = async (knex) => {
-  await knex.schema.table(transaction, (t) => {
+  await knex.schema.table(table, (t) => {
     t.index(['reference_id', 'purpose'])
   })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.table(transaction, (t) => {
+  await knex.schema.table(table, (t) => {
     t.dropIndex(['reference_id', 'purpose'])
   })
 }
