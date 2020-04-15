4143130d051edeb3e9e8ac19b2353f497a8eee52
Zeck Li <devformatters@gmail.com>
2020-04-15 19:29:34 +0800

    Add an index into article table.

--- db/migrations/20200415191540_add_index_to_article_table.js
+++ db/migrations/20200415191540_add_index_to_article_table.js
@@ -1,13 +1,13 @@
-const table_user = 'article'
+const table = 'article'
 
 exports.up = async (knex) => {
-  await knex.schema.table(table_user, (t) => {
+  await knex.schema.table(table, (t) => {
     t.index(['author_id', 'state'])
   })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.table(table_user, (t) => {
+  await knex.schema.table(table, (t) => {
     t.dropIndex(['author_id', 'state'])
   })
 }
