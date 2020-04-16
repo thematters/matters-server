4d04a831c829dd3930a907b21c67989c5ee6f1a1
Guo Liu <liuguomailbox@gmail.com>
2020-04-15 21:02:53 -0700

    drop constraint before adding

--- db/migrations/20200414153538_update_appreciation_constraint.js
+++ db/migrations/20200414153538_update_appreciation_constraint.js
@@ -18,16 +18,15 @@ exports.up = async (knex) => {
 
   await knex.raw(alterEnumString(table, 'type', ['LIKE', 'MAT']))
 
-  await knex.raw(/*sql*/ `
+  return knex.raw(/*sql*/ `
     alter table ${table}
       drop constraint if exists ${table}_sender_id_foreign,
       drop constraint if exists ${table}_recipient_id_foreign;
-  `)
 
-  return knex.schema.table(table, (t) => {
-    t.foreign('sender_id').references('user.id')
-    t.foreign('recipient_id').references('user.id')
-  })
+    alter table ${table}
+      add constraint ${table}_sender_id_foreign foreign key (sender_id) references user (id),
+      add constraint ${table}_recipient_id_foreign foreign key (recipient_id) references user (id);
+  `)
 }
 
 exports.down = (knex) =>
