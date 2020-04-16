6bf928019844025696c513b927f0f589e6477348
Guo Liu <liuguomailbox@gmail.com>
2020-04-15 20:18:16 -0700

    drop foreing if exists

--- db/migrations/20200414153538_update_appreciation_constraint.js
+++ db/migrations/20200414153538_update_appreciation_constraint.js
@@ -18,6 +18,12 @@ exports.up = async (knex) => {
 
   await knex.raw(alterEnumString(table, 'type', ['LIKE', 'MAT']))
 
+  await knex.raw(/*sql*/ `
+    alter table ${table}
+      drop constraint if exists ${table}_sender_id_foreign,
+      drop constraint if exists ${table}_recipient_id_foreign;
+  `)
+
   return knex.schema.table(table, (t) => {
     t.foreign('sender_id').references('user.id')
     t.foreign('recipient_id').references('user.id')
