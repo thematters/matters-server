7555303d501f3aa450c0e486d5fedaa416ff9642
Zeck Li <devformatters@gmail.com>
2020-04-22 17:12:27 +0800

    Add scripts of altering user table state enum, and revise migration util.

--- db/migrations/20200417153120_convert_user_state.js
+++ db/migrations/20200417153120_convert_user_state.js
@@ -1,5 +1,16 @@
+const { alterEnumString } = require('../utils')
+
+const table = 'user'
+
 exports.up = async (knex) => {
-  await knex('user').where({ state: 'frozen' }).update({ state: 'banned' })
+  await knex(table).where({ state: 'frozen' }).update({ state: 'banned' })
+  await knex.raw(
+    alterEnumString(table, 'state', ['onboarding', 'active', 'banned', 'archived'])
+  )
 }
 
-exports.down = async (knex) => {}
+exports.down = async (knex) => {
+  await knex.raw(
+    alterEnumString(table, 'state', ['onboarding', 'active', 'banned', 'frozen', 'archived'])
+  )
+}
