962e7a777f529a1ce88ea1e8446db65a9c4cfbea
Zeck Li <devformatters@gmail.com>
2020-04-29 18:59:03 +0800

    Revise calculation of ban expired date.

--- src/common/utils/time.ts
+++ src/common/utils/time.ts
@@ -1,11 +1,14 @@
+import { HOUR } from 'common/enums'
+
 /**
  * Get punish date exipired date (1 + n days).
  *
  */
 export const getPunishExpiredDate = (days: number) => {
   const offset = days > 0 ? 1 + days : 0
-  const date = new Date(Date.now())
-  date.setHours(0, 0, 0, 0)
+  const date = new Date(Date.now() + 8 * HOUR)
+  date.setUTCHours(0, 0, 0, 0)
   date.setDate(date.getDate() + offset)
+  date.setHours(date.getHours() - 8)
   return date
 }
