93ab043e6af260802b38197b74ae7857d2d8646e
Guo Liu <liuguomailbox@gmail.com>
2020-05-03 16:03:51 -0700

    add read_time to read count table

--- src/connectors/articleService.ts
+++ src/connectors/articleService.ts
@@ -1111,20 +1111,53 @@ export class ArticleService extends BaseService {
     articleId: string
     userId?: string | null
     ip?: string
-  }) =>
-    this.baseUpdateOrCreate({
-      data: {
-        articleId,
-        userId,
-        updated_at: new Date(),
-        count: this.knex.raw('count + 1'),
-        archived: false,
-        ip,
+  }) => {
+    const table = 'article_read_count'
+
+    const record = await this.baseFind({ where: { articleId, userId }, table })
+
+    const newData = {
+      articleId,
+      userId,
+      updatedAt: new Date(),
+      archived: false,
+      ip,
+    }
+
+    if (!record) {
+      // create new record
+      return this.baseCreate({ ...newData, count: 1, readTime: 0 }, table)
+    }
+
+    const oldData = record[0]
+
+    // lapsed time in secondes
+    const lapse = (Date.now() - new Date(oldData.updateAt).getTime()) / 1000
+
+    // after 30 minutes, accumulate count
+    if (lapse > 60 * 30) {
+      return this.baseUpdate(
+        oldData.id,
+        {
+          ...oldData,
+          ...newData,
+          count: oldData.count + 1,
+        },
+        table
+      )
+    }
+
+    // within 30 minutes, accumulate time
+    return this.baseUpdate(
+      oldData.id,
+      {
+        ...oldData,
+        ...newData,
+        readTime: oldData.readTime + lapse,
       },
-      where: { articleId, userId },
-      table: 'article_read_count',
-      createOptions: { count: 1 },
-    })
+      table
+    )
+  }
 
   /*********************************
    *                               *
