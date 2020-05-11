a93b1956dfd06f91d274d2999c5813301870059e
Zeck Li <devformatters@gmail.com>
2020-05-11 18:19:50 +0800

    Add  into transaction provider enums.

--- db/migrations/20200511181411_alter_transaction_provider_enum.js
+++ db/migrations/20200511181411_alter_transaction_provider_enum.js
@@ -3,7 +3,9 @@ const { alterEnumString } = require('../utils')
 const table = 'transaction'
 
 exports.up = async (knex) => {
-  await knex.raw(alterEnumString(table, 'provider', ['stripe', 'likecoin', 'matters']))
+  await knex.raw(
+    alterEnumString(table, 'provider', ['stripe', 'likecoin', 'matters'])
+  )
 }
 
 exports.down = async (knex) => {
