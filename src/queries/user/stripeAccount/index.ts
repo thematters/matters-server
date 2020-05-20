09bc123332016ff853d177598ad3f31ba29b230d
RoberTu <robertu0717@gmail.com>
2020-05-20 21:16:39 +0800

    revise "stripeAccount.id"

--- src/queries/user/stripeAccount/index.ts
+++ src/queries/user/stripeAccount/index.ts
@@ -3,7 +3,7 @@ import { GQLStripeAccountTypeResolver } from 'definitions'
 import loginUrl from './loginUrl'
 
 const StripeAccount: GQLStripeAccountTypeResolver = {
-  id: ({ id }) => id,
+  id: ({ accountId }) => accountId,
   loginUrl,
 }
 
