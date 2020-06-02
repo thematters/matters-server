a6a178eb0f33c43b4f0aba6d675d442023a4c355
Guo Liu <liuguomailbox@gmail.com>
2020-06-01 22:03:48 -0700

    add last read to article_read_count

--- db/migrations/20200601171831_add_timed_count_to_article_read_count.js
+++ db/migrations/20200601171831_add_timed_count_to_article_read_count.js
@@ -3,6 +3,7 @@ const table = 'article_read_count'
 exports.up = async (knex) => {
   await knex.schema.table(table, (t) => {
     t.bigInteger('timed_count').defaultTo(0)
+    t.timestamp('last_read').defaultTo()
   })
 
   await knex(table).update({
@@ -13,5 +14,6 @@ exports.up = async (knex) => {
 exports.down = async (knex) => {
   await knex.schema.table(table, (t) => {
     t.dropColumn('timed_count')
+    t.dropColumn('last_read')
   })
 }
