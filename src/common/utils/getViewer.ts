42ad45f525c2221c81c7f0dd00cace020d0102bf
RoberTu <robertu0717@gmail.com>
2020-07-22 13:19:56 +0800

    feat(public-private-queries): revert strip off viewer

--- src/common/utils/getViewer.ts
+++ src/common/utils/getViewer.ts
@@ -128,17 +128,8 @@ export const getViewerFromReq = async ({
   }
 
   // get user from token, use cookie first then 'x-access-token'
-  let token =
+  const token =
     cookie.parse(headers.cookie || '').token || headers['x-access-token'] || ''
-
-  // FIXME: Strip off viewer for query that name contains `Public` suffix,
-  // should be removed when the cookie-based token is deprecated
-  // @ts-ignore
-  const operationName = req?.query?.operationName || req?.body?.operationName
-  const isPublicQuery = /Public$/.test(operationName)
-  if (isPublicQuery) {
-    token = ''
-  }
 
   if (!token) {
     logger.info('User is not logged in, viewing as guest')
