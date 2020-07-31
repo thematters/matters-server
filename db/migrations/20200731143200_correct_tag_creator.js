c33c1cd76c6b0cdfe8b1f100ae6fcd7f2fff493c
Zeck Li <devformatters@gmail.com>
2020-07-31 13:58:41 +0800

    style(db): formatting

--- db/migrations/20200731143200_correct_tag_creator.js
+++ db/migrations/20200731143200_correct_tag_creator.js
@@ -1,7 +1,6 @@
 const table = 'tag'
 
 exports.up = async (knex) => {
-
   // reset matty's tags
   const matty = await knex('user')
     .select('id')
@@ -9,21 +8,70 @@ exports.up = async (knex) => {
     .first()
 
   const mattyTagIds = [
-    6898,  6899,  7114,  10167, 10446, 11631, 11633, 11708, 11838, 12778,
-    13004, 13006, 13145, 13699, 13796, 14631, 15017, 16421, 16679, 17273,
-    17503, 19034, 19296, 19497, 19756, 19862, 20833, 21371,
+    6898,
+    6899,
+    7114,
+    10167,
+    10446,
+    11631,
+    11633,
+    11708,
+    11838,
+    12778,
+    13004,
+    13006,
+    13145,
+    13699,
+    13796,
+    14631,
+    15017,
+    16421,
+    16679,
+    17273,
+    17503,
+    19034,
+    19296,
+    19497,
+    19756,
+    19862,
+    20833,
+    21371,
 
-    1211,  3937,  6025,  7727,  9549,  9913,  10249, 10431, 10537, 10716,
-    10989, 11168, 11354, 11492, 11528, 11650, 11705, 12691, 13482, 13704,
-    13801, 14105, 14428, 14547, 14551, 15137, 15493, 15608, 16209, 18832,
-    21292
+    1211,
+    3937,
+    6025,
+    7727,
+    9549,
+    9913,
+    10249,
+    10431,
+    10537,
+    10716,
+    10989,
+    11168,
+    11354,
+    11492,
+    11528,
+    11650,
+    11705,
+    12691,
+    13482,
+    13704,
+    13801,
+    14105,
+    14428,
+    14547,
+    14551,
+    15137,
+    15493,
+    15608,
+    16209,
+    18832,
+    21292,
   ]
 
-  for(const id of mattyTagIds) {
-    const tag = await knex(table)
-      .select('id', 'editors')
-      .where({ id })
-      .first()
+  for (const id of mattyTagIds) {
+    const tag = await knex(table).select('id', 'editors').where({ id }).first()
 
     if (!tag) {
       console.log(`tag: ${id}, not exist`)
@@ -39,17 +87,11 @@ exports.up = async (knex) => {
     console.log(`tag: ${id}, updated`)
   }
 
-
   // reset others' tags
-  const otherTagIds = [
-    11475, 12794, 15860, 16956, 18114, 19338
-  ]
+  const otherTagIds = [11475, 12794, 15860, 16956, 18114, 19338]
 
   for (const id of otherTagIds) {
-    const tag = await knex(table)
-      .select('id', 'editors')
-      .where({ id })
-      .first()
+    const tag = await knex(table).select('id', 'editors').where({ id }).first()
 
     if (!tag) {
       console.log(`tag: ${id}, not exist`)
