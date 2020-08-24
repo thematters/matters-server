e164695d8a7bdfaca9a00fd58404d3c17ef6ef2f
RoberTu <4065233+robertu7@users.noreply.github.com>
2020-08-24 10:24:01 +0800

    fix(recommendation): fix `random` index of `recommendation.authors`

--- src/queries/user/recommendation/authors.ts
+++ src/queries/user/recommendation/authors.ts
@@ -40,7 +40,6 @@ export const authors: RecommendationToAuthorsResolver = async (
   if (typeof filter?.random === 'number') {
     const MAX_RANDOM_INDEX = 50
     const randomDraw = first || 5
-    const index = Math.min(filter.random, MAX_RANDOM_INDEX)
 
     const authorPool = await userService.recommendAuthor({
       limit: MAX_RANDOM_INDEX * randomDraw,
@@ -48,11 +47,10 @@ export const authors: RecommendationToAuthorsResolver = async (
       oss,
     })
 
-    return connectionFromArray(
-      chunk(authorPool, randomDraw)[index],
-      input,
-      authorPool.length
-    )
+    const index = Math.min(filter.random, MAX_RANDOM_INDEX, chunk.length)
+    const filteredAuthors = chunk(authorPool, randomDraw)[index]
+
+    return connectionFromArray(filteredAuthors, input, authorPool.length)
   }
 
   const offset = cursorToIndex(after) + 1
