619e74fdb24ec1e38b66a7e92f5c6b06484e3295
Zeck Li <devformatters@gmail.com>
2020-09-04 11:45:54 +0800

    feat(db): correct article read record created during deployment

---
+++ db/migrations/20200904062843_correct_read_time_pt_2.js
@@ -0,0 +1,31 @@
+/**
+ * Fix read records that created in the middle of deployment
+ * of fixing wrong read_time in article_read_count.
+ */
+
+exports.up = async (knex) => {
+  await knex.raw(`
+    UPDATE
+        article_read_count
+    SET
+        read_time = ROUND(read_time::NUMERIC/1000)
+    FROM (
+        SELECT
+            base.*
+        FROM (
+            SELECT
+                id,
+                updated_at,
+                read_time / COALESCE(NULLIF(timed_count, 0), 1) AS avg_time
+            FROM
+                article_read_count
+        ) AS base
+        WHERE
+            base.updated_at >= '2020-07-31 12:30:00'
+            AND base.avg_time > 1800
+    ) AS source
+    WHERE article_read_count.id = source.id
+  `)
+}
+
+exports.down = async (knex) => {}
