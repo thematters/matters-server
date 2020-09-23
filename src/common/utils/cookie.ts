ee5ed3f51b47b9ceddda938f37b9bc8215e83e4b
RoberTu <4065233+robertu7@users.noreply.github.com>
2020-09-23 15:13:36 +0800

    feat(cors): disable samesite cookie for localhost

--- src/common/utils/cookie.ts
+++ src/common/utils/cookie.ts
@@ -7,12 +7,15 @@ import {
 import { isTest } from 'common/environment'
 
 const getCookieOption = (req: Request) => {
+  const origin = req.headers.origin || ''
+  const isLocalDev = /(localhost|127\.0\.0\.1):\d+$/.test(origin)
+
   return {
     maxAge: USER_ACCESS_TOKEN_EXPIRES_IN_MS,
     httpOnly: true,
     secure: req.protocol === 'https',
     domain: req.hostname,
-    sameSite: 'strict',
+    sameSite: isLocalDev ? undefined : 'strict',
   } as CookieOptions
 }
 
