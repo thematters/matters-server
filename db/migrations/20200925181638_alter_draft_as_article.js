c39776759b4ce4da8efc21d2b22806bbd16f443f
RoberTu <4065233+robertu7@users.noreply.github.com>
2020-09-25 20:35:29 +0800

    feat(article-versioning): alter draft table as article

---
+++ db/migrations/20200925181638_alter_draft_as_article.js
@@ -0,0 +1,53 @@
+const draft_table = 'draft'
+const article_table = 'article'
+
+exports.up = async (knex) => {
+  // Draft
+  await knex.schema.table(draft_table, (t) => {
+    // drop deprecated
+    t.dropColumn('scheduled_at')
+
+    // new from article
+    t.integer('word_count')
+    t.string('data_hash')
+    t.string('media_hash')
+    t.string('language').defaultTo()
+
+    // versioning
+    t.bigInteger('prev_draft_id').unsigned()
+    t.bigInteger('article_id').unsigned()
+
+    t.foreign('prev_draft_id').references('id').inTable(draft_table)
+    t.foreign('article_id').references('id').inTable(article_table)
+
+    // indexes
+    t.index(['prev_draft_id', 'article_id'])
+  })
+
+  // Article
+  await knex.schema.table(article_table, (t) => {
+    t.index('draft_id')
+  })
+}
+
+exports.down = async (knex) => {
+  // Article
+  await knex.schema.table(article_table, (t) => {
+    t.dropIndex('draft_id')
+  })
+
+  // Draft
+  await knex.schema.table(draft_table, (t) => {
+    t.dropIndex(['prev_draft_id', 'article_id'])
+
+    t.dropColumn('article_id')
+    t.dropColumn('prev_draft_id')
+
+    t.dropColumn('language')
+    t.dropColumn('media_hash')
+    t.dropColumn('data_hash')
+    t.dropColumn('word_count')
+
+    t.timestamp('scheduled_at')
+  })
+}
