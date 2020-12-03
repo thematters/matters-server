9638c156dff001273cd4c1929b640adf5852b034
RoberTu <4065233+robertu7@users.noreply.github.com>
2020-12-03 10:21:15 +0800

    fix(draft): fix self-closing tag exists in draft content

--- src/common/utils/content.ts
+++ src/common/utils/content.ts
@@ -1,4 +1,5 @@
 import * as cheerio from 'cheerio'
+import flow from 'lodash/flow'
 
 export const stripHtml = (html: string, replacement = ' ') =>
   (String(html) || '')
@@ -61,4 +62,21 @@ export const stripClass = (html: string, name: string) => {
   $(`.${name}`).removeClass(name)
 
   return $.html()
+}
+
+/**
+ * Correct self-closing tag
+ */
+export const correctSelfClosingHtmlTag = (name: string) => (html: string) => {
+  const pattern = new RegExp(`<${name}(.*?)\s*\/>`, 'g')
+  const replacement = `<${name}$1></${name}>`
+  return (html || '').replace(pattern, replacement)
+}
+
+/**
+ * Pipe for pre-processing html tag.
+ */
+export const correctHtml = (html: string) => {
+  const pipe = flow(correctSelfClosingHtmlTag('iframe'))
+  return pipe(html)
 }
