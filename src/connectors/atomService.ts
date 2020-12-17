8b50b49ce081eea8aa49e4a0c2c36a59ae91f109
Zeck Li <devformatters@gmail.com>
2020-12-17 12:04:05 +0800

    refactor(service): move methods from service to resolver

--- src/connectors/atomService.ts
+++ src/connectors/atomService.ts
@@ -3,6 +3,7 @@ import DataLoader from 'dataloader'
 import Knex from 'knex'
 
 import { EntityNotFoundError } from 'common/errors'
+import logger from 'common/logger'
 import { aws, es, knex } from 'connectors'
 import { Item, TableName } from 'definitions'
 
@@ -22,21 +23,29 @@ interface UpdateInput {
   data: Record<string, any>
 }
 
+interface DeleteManyInput {
+  table: TableName
+  where?: Record<string, any>
+  whereIn?: [string, string[]]
+}
+
 /**
  * This object is a container for data loaders or system wide services.
  */
 export class AtomService extends DataSource {
+  es: typeof es
   knex: Knex
 
   constructor() {
     super()
+    this.es = es
     this.knex = knex
   }
 
   /**
    * Find an unique record.
    *
-   * A Prisma like query for retrieving a record by specified id.
+   * A Prisma like method for retrieving a record by specified id.
    */
   findUnique = async ({ table, where }: FindInput) =>
     this.knex.select().from(table).where(where).first()
@@ -44,7 +53,7 @@ export class AtomService extends DataSource {
   /**
    * Update an unique record.
    *
-   * A Prisma like operation for updating a record by specified id.
+   * A Prisma like method for updating a record by specified id.
    */
   update = async ({ table, where, data }: UpdateInput) => {
     const [record] = await this.knex
@@ -53,5 +62,33 @@ export class AtomService extends DataSource {
       .into(table)
       .returning('*')
     return record
+  }
+
+  /**
+   * Delete records.
+   *
+   * A Prisma like method for deleting multiple records.
+   */
+  deleteMany = async ({ table, where, whereIn }: DeleteManyInput) => {
+    const action = this.knex(table)
+    if (where) {
+      action.where(where)
+    }
+    if (whereIn) {
+      action.whereIn(...whereIn)
+    }
+    await action.del()
+  }
+
+  /**
+   * Delete data stored in elastic search.
+   */
+  deleteSearch = async ({ table, id }: { table: TableName; id: any }) => {
+    try {
+      const result = await this.es.client.delete({ index: table, id })
+      return result
+    } catch (error) {
+      logger.error(error)
+    }
   }
 }
