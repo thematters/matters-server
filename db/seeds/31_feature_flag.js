8c6672bdd858eb901822eecb1632881f1b11b9ad
Zeck Li <devformatters@gmail.com>
2021-01-28 17:27:53 +0800

    test(feature-flag): add seeding data for feature flag

---
+++ db/seeds/31_feature_flag.js
@@ -0,0 +1,35 @@
+const table = 'feature_flag'
+
+exports.seed = async (knex) => {
+  await knex(table).del()
+  await knex(table).insert([
+    {
+      name: 'add_credit',
+      flag: 'on',
+    },
+    {
+      name: 'circle_management',
+      flag: 'on',
+    },
+    {
+      name: 'circle_interact',
+      flag: 'on',
+    },
+    {
+      name: 'fingerprint',
+      flag: 'on',
+    },
+    {
+      name: 'payment',
+      flag: 'on',
+    },
+    {
+      name: 'payout',
+      flag: 'on',
+    },
+    {
+      name: 'verify_appreciate',
+      flag: 'off',
+    },
+  ])
+}
