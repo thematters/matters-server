78d73b007b25370861940e4e2e4706d7de2dded6
Zeck Li <devformatters@gmail.com>
2021-03-11 17:12:00 +0800

    fix(error): return right error for subscription

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -466,3 +466,11 @@ export class DuplicateCircleError extends ApolloError {
     Object.defineProperty(this, 'name', { value: 'DuplicateCircleError' })
   }
 }
+
+export class DuplicateCircleSubscriptionError extends ApolloError {
+  constructor(message: string) {
+    super(message, 'DUPLICATE_CIRCLE_SUBSCRIPTION')
+
+    Object.defineProperty(this, 'name', { value: 'DuplicateCircleSubscriptionError' })
+  }
+}
