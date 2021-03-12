85148fde4ed6725989551049c5fb768c21efd92d
Zeck Li <devformatters@gmail.com>
2021-03-12 16:34:11 +0800

    fix(circle): add reserved circle names

--- src/common/enums/hardcode.ts
+++ src/common/enums/hardcode.ts
@@ -7,4 +7,28 @@ export const RESERVED_NAMES = [
   '张洁平',
 ]
 
+export const RESERVED_CIRCLE_NAMES = [
+  'about',
+  'about_us',
+  'admin',
+  'administrator',
+  'circle',
+  'community',
+  'contact',
+  'contact_us',
+  'guide',
+  'home',
+  'info',
+  'information',
+  'matty',
+  'migration',
+  'office',
+  'official',
+  'official_circle',
+  'official_matty',
+  'team',
+  'teams',
+  'tos',
+]
+
 export const AUTO_FOLLOW_TAGS = ['Matters新人打卡', '玩轉Matters實用指南']
