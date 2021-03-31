8d7242826c86e8d3e7169290bb6763fd6a7dbd22
Zeck Li <devformatters@gmail.com>
2021-03-31 16:00:56 +0800

    fix(invitation): fix `invitedBy` resolver not including person invited via email

--- src/queries/circle/invitedBy.ts
+++ src/queries/circle/invitedBy.ts
@@ -3,16 +3,20 @@ import { CircleToInvitedByResolver } from 'definitions'
 const resolver: CircleToInvitedByResolver = async (
   { id },
   _,
-  { dataSources: { atomService }, viewer }
+  { knex, viewer }
 ) => {
   if (!viewer.id) {
     return null
   }
 
-  const invitation = await atomService.findFirst({
-    table: 'circle_invitation',
-    where: { circleId: id, userId: viewer.id },
-  })
+  const invitation = await knex
+    .select()
+    .from('circle_invitation')
+    .where({ circleId: id })
+    .andWhere(function () {
+      this.where('user_id', viewer.id).orWhere('email', viewer.email)
+    })
+    .first()
 
   return invitation
 }
