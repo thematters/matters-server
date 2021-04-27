46bdf533b6021884d78571b950a54bfb8e87b3aa
Zeck Li <devformatters@gmail.com>
2021-04-27 22:39:49 +0800

    feat(migrate): print info to console

--- db/migrations/20210427123109_migrate_subscription.js
+++ db/migrations/20210427123109_migrate_subscription.js
@@ -43,12 +43,19 @@ exports.up = async (knex) => {
     .select()
     .from(t_invitation)
     .where({ accepted: true })
+    .andWhereNull('subscription_item_id')
     .orderBy('accepted_at', 'asc')
 
-  for (const invt of invts) {
+  const total = (invts || []).length
+
+  for (const [index, invt] of invts.entries()) {
+    console.log('-------------------------------')
+    console.log(`Process ${index + 1}/${total} invitation: ${invt.id}`)
+
     const { user_id: userId, email } = invt
 
     if (!userId && !email) {
+      console.log('User id and email are not provided.')
       continue
     }
 
@@ -58,8 +65,10 @@ exports.up = async (knex) => {
     })
 
     if (!user) {
+      console.log('User not found.')
       continue
     }
+    console.log(`Invitation user: ${user.id}`)
 
     // Step 1: find matched subscription item
     const item = await knex
@@ -76,8 +85,10 @@ exports.up = async (knex) => {
       .first()
 
     if (!item) {
+      console.log('Subscription item not found.')
       continue
     }
+    console.log(`Subscription item: ${item.id}`)
 
     // Step 2: create Matters subscription if it doesn't exist
     const subscription = await knex
@@ -88,8 +99,10 @@ exports.up = async (knex) => {
       .first()
 
     if (!subscription) {
+      console.log('Subscription not found.')
       continue
     }
+    console.log(`Subscription: ${subscription.id}`)
 
     let mattersSubscription = await findFirst({
       table: t_subscription,
@@ -106,19 +119,23 @@ exports.up = async (knex) => {
           user_id: user.id,
         },
       })
+      console.log(`Inserted Matters subscription: ${mattersSubscription.id}`)
     }
+    console.log(`Matters subscription: ${mattersSubscription.id}`)
 
     // Step 3: create Matters subscription item
-    const { id, circle_id, ...rest } = item
     const mattersItem = await insert({
       table: t_subscription_item,
       data: {
-        ...rest,
+        user_id: item.user_id,
+        subscription_id: mattersSubscription.id,
+        price_id: item.price_id,
         provider: 'matters',
         provider_subscription_item_id: v4(),
-        subscription_id: mattersSubscription.id,
+        archived: false,
       },
     })
+    console.log(`Inserted Matters subscription item: ${mattersItem.id}`)
 
     // Step 4: update invitation with newly added subscription item
     await update({
@@ -126,6 +143,9 @@ exports.up = async (knex) => {
       where: { id: invt.id },
       data: { subscription_item_id: mattersItem.id },
     })
+    console.log(
+      `Updated subscription item (${mattersItem.id}) to invitation (${invt.id}`
+    )
 
     // Step 5: remove Stripe subscription item, including Stripe subscription if it has no or only one item
     let canceledStripeSubscription
@@ -138,20 +158,34 @@ exports.up = async (knex) => {
         stripeItems.data.length === 1 &&
         stripeItems.data[0].id === item.provider_subscription_item_id
       ) {
+        console.log(
+          `Cancel entire Stripe subscription: ${subscription.provider_subscription_id}`
+        )
         canceledStripeSubscription = await stripeAPI.subscriptions.del(
           subscription.provider_subscription_id,
           { prorate: false }
         )
+        console.log('Cancel completed')
       } else if (stripeItems.data.length > 1) {
+        console.log(
+          `Remove Stripe subscription item: ${item.provider_subscription_item_id}`
+        )
         await stripeAPI.subscriptionItems.del(
           item.provider_subscription_item_id,
           { proration_behavior: 'none' }
+        )
+        console.log('Remove completed')
+        console.log(
+          `Update Stripe subscription with empty coupon: ${subscription.provider_subscription_id}`
         )
         await stripeAPI.subscriptions.update(
           subscription.provider_subscription_id,
           { coupon: '' }
         )
+        console.log('Update completed')
       }
+    } else {
+      console.log('No Stripe subscription items need to update')
     }
 
     // Step 6: archive Matters-Stripe subscription item
@@ -160,6 +194,7 @@ exports.up = async (knex) => {
       where: { id: item.id },
       data: { archived: true, remark: 'trial_migration' },
     })
+    console.log(`Upadted matters-stripe subscription item: ${item.id}`)
 
     // Step 7: update Matters-Stripe subscription
     if (canceledStripeSubscription) {
@@ -172,6 +207,9 @@ exports.up = async (knex) => {
       const count = parseInt(record ? `${record.count}` : '0', 10)
 
       if (count > 0) {
+        console.log(
+          `Subscription has other items, won't cancel: ${subscription.id}`
+        )
         continue
       }
 
@@ -186,6 +224,9 @@ exports.up = async (knex) => {
           updated_at: new Date(),
         },
       })
+      console.log(
+        `Updated subscription status and action times: ${subscription.id}`
+      )
     }
   }
 }
