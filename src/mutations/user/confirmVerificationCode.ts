6f2fa52c634c7cfa70e796cdb9de8f7ace255458
Zeck Li <devformatters@gmail.com>
2021-07-01 18:04:43 +0800

    fix(code): better error code retruning in confirmation

--- src/mutations/user/confirmVerificationCode.ts
+++ src/mutations/user/confirmVerificationCode.ts
@@ -1,18 +1,37 @@
+import { VERIFICATION_CODE_STATUS } from 'common/enums'
 import { CodeExpiredError, CodeInvalidError } from 'common/errors'
 import { MutationToConfirmVerificationCodeResolver } from 'definitions'
 
 const resolver: MutationToConfirmVerificationCodeResolver = async (
   _,
   { input },
-  { dataSources: { userService } }
+  { dataSources: { atomService, userService } }
 ) => {
   const { email: rawEmail } = input
   const email = rawEmail ? rawEmail.toLowerCase() : null
-  const [code] = await userService.findVerificationCodes({
-    where: { ...input, email, status: 'active' },
+
+  // fetch the latest code sent to the email
+  const [code] = await atomService.findMany({
+    table: 'verification_code',
+    where: { ...input, email },
+    orderBy: [{ column: 'id', order: 'desc'}],
+    take: 1,
   })
 
-  if (!code) {
+  if (
+    !code ||
+    code.status === VERIFICATION_CODE_STATUS.inactive ||
+    code.status === VERIFICATION_CODE_STATUS.verified ||
+    code.status === VERIFICATION_CODE_STATUS.used
+  ) {
+    throw new CodeInvalidError('code does not exists')
+  }
+
+  if (code.status === VERIFICATION_CODE_STATUS.expired) {
+    throw new CodeExpiredError('code is exipred')
+  }
+
+  if (code.status !== VERIFICATION_CODE_STATUS.active) {
     throw new CodeInvalidError('code does not exists')
   }
 
