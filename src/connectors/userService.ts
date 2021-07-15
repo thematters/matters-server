7e5cb4dc99eb6558fb49a70412f8adbf0fabe4a4
robertu <4065233+robertu7@users.noreply.github.com>
2021-07-15 09:49:32 +0000

    feat(schema): sign jwt with user id instead of uuid

--- src/connectors/userService.ts
+++ src/connectors/userService.ts
@@ -140,7 +140,7 @@ export class UserService extends BaseService {
 
     await this.verifyPassword({ password, hash: user.passwordHash })
 
-    const token = jwt.sign({ uuid: user.uuid }, environment.jwtSecret, {
+    const token = jwt.sign({ id: user.id }, environment.jwtSecret, {
       expiresIn: USER_ACCESS_TOKEN_EXPIRES_IN_MS / 1000,
     })
 
