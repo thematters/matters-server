1281649771eea599873366c6ae00ac9f9b9302ed
robertu <4065233+robertu7@users.noreply.github.com>
2021-08-12 12:17:44 +0000

    feat(circle): add subscriber.inviteeHistory and subscriber.subscriberHistory resolvers

--- src/queries/circle/analytics/subscriber/inviteeHistory.ts
+++ src/queries/circle/analytics/subscriber/inviteeHistory.ts
@@ -1,6 +1,3 @@
-import Knex from 'knex'
-
-import { PAYMENT_PROVIDER, PRICE_STATE, SUBSCRIPTION_STATE } from 'common/enums'
 import { CircleSubscriberAnalyticsToInviteeHistoryResolver } from 'definitions'
 
 const resolver: CircleSubscriberAnalyticsToInviteeHistoryResolver = async (
@@ -8,57 +5,47 @@ const resolver: CircleSubscriberAnalyticsToInviteeHistoryResolver = async (
   _,
   { dataSources: { atomService }, knex }
 ) => {
-  const take = 4
-
   const selectPastMonth = (month: number) =>
     knex.raw(
       `select date_trunc('month', current_date - interval '${month}' month) as date`
     )
-  const result = await knex
-    .with('last_4_months', (builder) => {
-      builder
-        .select()
-        .union([
-          selectPastMonth(3),
-          selectPastMonth(2),
-          selectPastMonth(1),
-          selectPastMonth(0),
-        ])
-        .orderBy('date', 'asc')
-    })
-    .select('date')
-    .select(knex.raw(`sum(value) OVER (ORDER BY date asc) AS value`))
-    .from((builder: Knex.QueryBuilder) => {
-      builder
-        .select(knex.raw(`coalesce(date, group_month) as date`))
-        .select(knex.raw(`coalesce(group_count, 0) as value`))
-        .from('last_4_months')
-        .fullOuterJoin(
-          knex
-            .select(knex.raw(`date_trunc('month', created_at) as group_month`))
-            .from('circle_subscription_item as csi')
-            .join('circle_price', 'circle_price.id', 'csi.price_id')
-            .join('circle_subscription as cs', 'cs.id', 'csi.subscription_id')
-            .where({
-              'circle_price.circle_id': id,
-              'circle_price.state': PRICE_STATE.active,
-              'csi.provider': PAYMENT_PROVIDER.matters,
-              'csi.archived': false,
-            })
-            .whereIn('cs.state', [
-              SUBSCRIPTION_STATE.active,
-              SUBSCRIPTION_STATE.trialing,
-            ])
-            .groupBy('group_month')
-            .as('history'),
-          'history.group_month',
-          'last_4_months.date'
+  const [last4Months, invitations] = await Promise.all([
+    knex
+      .select()
+      .union([
+        selectPastMonth(3),
+        selectPastMonth(2),
+        selectPastMonth(1),
+        selectPastMonth(0),
+      ])
+      .orderBy('date', 'asc'),
+    knex
+      .count('id')
+      .select(knex.raw(`date_trunc('month', accepted_at) as started_at`))
+      .select(
+        knex.raw(
+          `date_trunc('month', accepted_at + duration_in_days * interval '1 day') as ended_at`
         )
-        .as('joined_history')
-    })
-    .as('cumsum_history')
+      )
+      .from('circle_invitation')
+      .where({
+        circle_id: id,
+      })
+      .whereNotNull('accepted_at')
+      .groupBy('started_at', 'ended_at'),
+  ])
 
-  return result.slice(take * -1)
+  return last4Months.map(({ date }) => {
+    const value = invitations.reduce((a, c) => {
+      if (date >= c.startedAt && date <= c.endedAt) {
+        return a + parseInt(c.count, 10) || 0
+      }
+
+      return a
+    }, 0)
+
+    return { date, value }
+  })
 }
 
 export default resolver
--- src/queries/circle/analytics/subscriber/subscriberHistory.ts
+++ src/queries/circle/analytics/subscriber/subscriberHistory.ts
@@ -1,3 +1,4 @@
+import { PAYMENT_PROVIDER } from 'common/enums'
 import { CircleSubscriberAnalyticsToSubscriberHistoryResolver } from 'definitions'
 
 const resolver: CircleSubscriberAnalyticsToSubscriberHistoryResolver = async (
@@ -5,7 +6,48 @@ const resolver: CircleSubscriberAnalyticsToSubscriberHistoryResolver = async (
   _,
   { dataSources: { atomService }, knex }
 ) => {
-  return [{ value: 0, date: new Date() }]
+  const selectPastMonth = (month: number) =>
+    knex.raw(
+      `select date_trunc('month', current_date - interval '${month}' month) as date`
+    )
+  const [last4Months, subItems] = await Promise.all([
+    knex
+      .select()
+      .union([
+        selectPastMonth(3),
+        selectPastMonth(2),
+        selectPastMonth(1),
+        selectPastMonth(0),
+      ])
+      .orderBy('date', 'asc'),
+    knex
+      .count('csi.id')
+      .select(knex.raw(`date_trunc('month', csi.created_at) as started_at`))
+      .select(
+        knex.raw(
+          `(CASE csi.archived WHEN false THEN null ELSE date_trunc('month', csi.updated_at) END) as ended_at`
+        )
+      )
+      .from('circle_subscription_item as csi')
+      .leftJoin('circle_price', 'csi.price_id', 'circle_price.id')
+      .where({
+        'circle_price.circle_id': id,
+        'csi.provider': PAYMENT_PROVIDER.stripe,
+      })
+      .groupBy('started_at', 'ended_at'),
+  ])
+
+  return last4Months.map(({ date }) => {
+    const value = subItems.reduce((a, c) => {
+      if (date >= c.startedAt && (!c.endedAt || date <= c.endedAt)) {
+        return a + parseInt(c.count, 10) || 0
+      }
+
+      return a
+    }, 0)
+
+    return { date, value }
+  })
 }
 
 export default resolver
