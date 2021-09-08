e70186fa0ddc80ffd08a47b6bbbf50a0026681e4
robertu <4065233+robertu7@users.noreply.github.com>
2021-09-08 09:09:12 +0000

    feat(rebase): rebase on feature/user-profile

--- src/queries/article/chapter/articles.ts
+++ src/queries/article/chapter/articles.ts
@@ -3,13 +3,17 @@ import { ChapterToArticlesResolver } from 'definitions'
 const resolver: ChapterToArticlesResolver = async (
   { id: chapterId },
   _,
-  { dataSources: { atomService } }
+  { dataSources: { atomService, articleService } }
 ) => {
-  return atomService.findMany({
+  const chapterArticles = await atomService.findMany({
     table: 'article_chapter',
     where: { chapterId },
     orderBy: [{ column: 'order', order: 'asc' }],
   })
+
+  return articleService.draftLoader.loadMany(
+    chapterArticles.map((item) => item.articleId)
+  )
 }
 
 export default resolver
--- src/queries/article/topic/articles.ts
+++ src/queries/article/topic/articles.ts
@@ -3,13 +3,17 @@ import { TopicToArticlesResolver } from 'definitions'
 const resolver: TopicToArticlesResolver = async (
   { id: topicId },
   _,
-  { dataSources: { atomService } }
+  { dataSources: { atomService, articleService } }
 ) => {
-  return atomService.findMany({
+  const topicArticles = await atomService.findMany({
     table: 'article_topic',
     where: { topicId },
     orderBy: [{ column: 'order', order: 'asc' }],
   })
+
+  return articleService.draftLoader.loadMany(
+    topicArticles.map((item) => item.articleId)
+  )
 }
 
 export default resolver
