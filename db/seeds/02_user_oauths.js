4e285ad1dfa02d6c5e4616e32cfd09d0742f3e4a
robertu <4065233+robertu7@users.noreply.github.com>
2021-10-22 07:52:09 +0000

    feat(likecoin): add seeds for liker_id

--- db/seeds/02_user_oauths.js
+++ db/seeds/02_user_oauths.js
@@ -1,9 +1,24 @@
 const table = 'user_oauth'
+const table_likecoin = 'user_oauth_likecoin'
 
 exports.seed = function (knex, Promise) {
   return knex(table)
     .del()
     .then(function () {
       return knex(table).insert([])
+    })
+    .then(function () {
+      return knex(table_likecoin)
+        .del()
+        .then(function () {
+          return knex(table_likecoin).insert([
+            {
+              liker_id: 'test_liker_id',
+              account_type: 'general',
+              access_token: '123123',
+              refresh_token: '123123',
+            },
+          ])
+        })
     })
 }
