ad799203e298a7c1109c3b8a85a794d2cb9eb0e9
carolusian <pkufashuo400@gmail.com>
2022-04-17 23:32:54 +0800

    fix: handle mixed languages

--- src/common/utils/content.ts
+++ src/common/utils/content.ts
@@ -4,14 +4,20 @@ import cloneDeep from 'lodash/cloneDeep'
 import flow from 'lodash/flow'
 
 export const countWords = (html: string) => {
+  // Chinese(\u4e00-\u9fcc); Korean(\uac00-\ud7af); Japanese(\u3040-\u309f\u30a0-\u30ff); Russian([\u0401\u0451\u0410-\u044f]+)
+  const regex =
+    /[\u4e00-\u9fcc]|[\uac00-\ud7af]|[\u3040-\u309f\u30a0-\u30ff]|[\u0401\u0451\u0410-\u044f]+|\w+/g
   const content = stripHtml(html).trim()
-  const matches = content.match(/[\u4e00-\u9fcc]|\w+/g)
+  const matches = content.match(regex)
 
-  if (matches) {
-    return matches.length
+  const matchesLen = matches ? matches.length : 0
+  const splitLen = content.split(/\s+/g).length
+
+  if (matchesLen >= splitLen) {
+    return matchesLen
   } else if (content.length > 0) {
-    // fallback for characters outside the range defined in regular expression
-    return content.split(/\s+/g).length
+    // fallback
+    return splitLen
   } else {
     return 0
   }
