0bda5270d78c84cfef863e7fe7b885b2999b4e37
49659410+tx0c <>
2022-05-10 22:16:20 +0000

    feat(cors-allow-vercel-preview): part-i: [Server] - CORS readonly
    resolves #2534

--- src/common/enums/cors.ts
+++ src/common/enums/cors.ts
@@ -1,18 +1,20 @@
 import { CorsOptions } from 'cors'
 
+import { isProd } from 'common/environment'
+
 export const CORS_OPTIONS: CorsOptions = {
   origin: (origin, callback) => {
     if (!origin) {
       return callback(null, false)
     }
 
-    const isLocalDev =
-      /(localhost|127\.0\.0\.1):\d+$/.test(origin) ||
-      /githubpreview\.dev$/.test(origin)
+    const isLocalDev = /(localhost|127\.0\.0\.1):\d+$/.test(origin)
     const isMatters = /\/\/(.*\.)?matters\.news$/.test(origin)
     const isApolloStudio = /\/\/(.*\.)?apollographql\.com$/.test(origin)
-    const isVercelPreview = /\.vercel\.app$/.test(origin)
-    const isAllowed = isLocalDev || isMatters || isApolloStudio || isVercelPreview
+    const isDevPreview =
+      !isProd &&
+      (/\.vercel\.app$/.test(origin) || /githubpreview\.dev$/.test(origin))
+    const isAllowed = isLocalDev || isMatters || isApolloStudio || isDevPreview
 
     callback(null, isAllowed)
   },
