af6f47c10a27230563f35b0712ae3543f23c7c91
carolusian <pkufashuo400@gmail.com>
2022-06-22 06:16:29 +0800

    feat: add reset crypto wallet API

---
+++ src/mutations/user/resetWallet.ts
@@ -0,0 +1,26 @@
+import { ForbiddenError } from 'common/errors'
+import { fromGlobalId } from 'common/utils'
+import { MutationToResetWalletResolver } from 'definitions'
+
+const resolver: MutationToResetWalletResolver = async (
+  root,
+  { input: { id } },
+  { viewer, dataSources: { atomService, userService } }
+) => {
+  const { id: dbId } = fromGlobalId(id)
+  const user = await userService.dataloader.load(dbId)
+
+  if (!user || !user.ethAddress) {
+    throw new ForbiddenError("user doesn't exist or have a crypto wallet")
+  }
+
+  const updatedUser = await atomService.update({
+    table: 'user',
+    where: { id: user.id },
+    data: { updatedAt: new Date(), ethAddress: null },
+  })
+
+  return updatedUser
+}
+
+export default resolver
