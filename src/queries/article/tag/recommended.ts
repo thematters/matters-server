f42410c158f2d454475caeaf49381a691996ce46
Aka jazzy <mingchenliang954@gmail.com>
2022-08-15 22:04:25 +0800

    feat(related-tags): format recommended.ts file

--- src/queries/article/tag/recommended.ts
+++ src/queries/article/tag/recommended.ts
@@ -1,4 +1,5 @@
 import { chunk } from 'lodash'
+
 import { TAGS_RECOMMENDED_LIMIT } from 'common/enums'
 import {
   connectionFromArray,
@@ -12,39 +13,46 @@ const resolver: TagToRecommendedResolver = async (
   { input },
   { dataSources: { tagService, userService } }
 ) => {
-
   const { take, skip } = fromConnectionArgs(input, {
-    allowTakeAll: true, 
+    allowTakeAll: true,
     maxTake: TAGS_RECOMMENDED_LIMIT,
     maxSkip: TAGS_RECOMMENDED_LIMIT,
-    })
+  })
+
   if (!id) {
     return connectionFromArray([], input)
   }
   const { filter } = input
-
 
   if (typeof filter?.random === 'number') {
     const { random } = filter
     const draw = input.first || 5
     const limit = 50
 
-  //take (limit * draw) amounts of related tags
-  const related = await tagService.findRelatedTags({ id, content, take: limit * draw, skip })
-  //chunk the list of tags per draw
-  const chunks = chunk(related, draw)
-  const index = Math.min(random, limit, chunks.length - 1)
-  const filteredTags = chunks[index] || []
+    // take (limit * draw) amounts of related tags
+    const related = await tagService.findRelatedTags({
+      id,
+      content,
+      take: limit * draw,
+      skip,
+    })
+    // chunk the list of tags per draw
+    const chunks = chunk(related, draw)
+    const index = Math.min(random, limit, chunks.length - 1)
+    const filteredTags = chunks[index] || []
 
+    return connectionFromPromisedArray(
+      tagService.dataloader.loadMany(
+        filteredTags.map((tag: any) => `${tag.id}`)
+      ),
+      input,
+      related.length
+    )
+  }
   return connectionFromPromisedArray(
-    tagService.dataloader.loadMany(
-      filteredTags.map((tag: any) => `${tag.id}`)
-    ),
-    input,
-    related.length
+    tagService.findRelatedTags({ id, content, take, skip }),
+    input
   )
-}
-  return connectionFromPromisedArray(tagService.findRelatedTags({ id, content, take, skip }), input)
 }
 
 export default resolver
