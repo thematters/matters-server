b9f268d5c615a63b78b3802bc899b1e3c70e2b35
xdj <xudongjie@laixi.pro>
2022-12-14 17:36:58 +0800

    fix(migration): fix migration script to avoid tests failure;

--- db/migrations/20221214133648_fix_user_last_seen_value.js
+++ db/migrations/20221214133648_fix_user_last_seen_value.js
@@ -2,8 +2,14 @@ const { baseDown } = require('../utils')
 
 exports.up = async (knex) => {
   await knex.raw(/*sql*/ `
-  UPDATE public.user SET last_seen = users_lasts.last_at
-      FROM mat_views.users_lasts AS users_lasts WHERE public.user.id = users_lasts.id;
+  DO $$
+  BEGIN
+  IF EXISTS (SELECT 1 FROM pg_views WHERE viewname = 'users_lasts') THEN
+      UPDATE public.user SET last_seen = users_lasts.last_at
+          FROM mat_views.users_lasts AS users_lasts WHERE public.user.id = users_lasts.id;
+  END IF;
+  END;
+  $$
   `)
 }
 
