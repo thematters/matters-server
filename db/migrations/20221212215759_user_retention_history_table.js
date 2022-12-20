27ddc7c87e70eb3c9fcb8fdcf8df4462d4ede79f
xdj <xudongjie@laixi.pro>
2022-12-20 21:51:43 +0800

    style: minor

--- db/migrations/20221212215759_user_retention_history_table.js
+++ db/migrations/20221212215759_user_retention_history_table.js
@@ -7,13 +7,7 @@ exports.up = async (knex) => {
   await knex.schema.createTable(table, (t) => {
     t.bigIncrements('id').primary()
     t.bigInteger('user_id').unsigned().notNullable()
-    t.enu('state', [
-      'NEWUSER',
-      'ALERT',
-      'INACTIVE',
-      'NORMAL',
-      'ACTIVE',
-    ])
+    t.enu('state', ['NEWUSER', 'ALERT', 'INACTIVE', 'NORMAL', 'ACTIVE'])
     t.timestamp('created_at').defaultTo(knex.fn.now())
 
     t.foreign('user_id').references('id').inTable('user')
