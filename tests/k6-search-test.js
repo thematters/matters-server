c2ef552bf49a1efc86cf54b7abd48d64df6b94d9
xdj <xudongjie@laixi.pro>
2023-01-04 15:29:30 +0800

    refactor(k6-script): minor

--- tests/k6-search-test.js
+++ tests/k6-search-test.js
@@ -1,58 +1,62 @@
-import http from "k6/http";
-import { SharedArray } from "k6/data";
-import { check, fail } from "k6";
+import http from 'k6/http'
+import { SharedArray } from 'k6/data'
+import { check, fail } from 'k6'
+
+// usage:
+// k6 run -e RATE=1 -e TYPE=User -e VERSION=v20221212 -e TARGET=dev --duration=30s k6-search-test.js
 
 // env
-const target = __ENV.TARGET || "dev"; // or 'prod'
+const target = __ENV.TARGET || 'dev' // or 'prod'
+const queryType = __ENV.TYPE || 'User'
+const queryVersion = __ENV.VERSION || 'v20221212'
+const rate = +__ENV.RATE || 1
+
 const url =
-  target === "dev"
-    ? "https://server-develop.matters.news/graphql"
-    : "http://matters-server-prod.ap-southeast-1.elasticbeanstalk.com/graphql";
-const queryType = __ENV.TYPE || "User";
-const queryVersion = __ENV.VERSION || "v20221212";
-const rate = +__ENV.RATE || 1;
+  target === 'dev'
+    ? 'https://server-develop.matters.news/graphql'
+    : 'http://matters-server-prod.ap-southeast-1.elasticbeanstalk.com/graphql'
 
 export function setup() {
-  console.log({ url, queryType, queryVersion, rate });
+  console.log({ url, queryType, queryVersion, rate })
 }
 
 export const options = {
   summaryTrendStats: [
-    "avg",
-    "min",
-    "med",
-    "max",
-    "p(95)",
-    "p(99)",
-    "p(99.99)",
-    "count",
+    'avg',
+    'min',
+    'med',
+    'max',
+    'p(95)',
+    'p(99)',
+    'p(99.99)',
+    'count',
   ],
   scenarios: {
     contacts: {
-      executor: "constant-arrival-rate",
-      duration: "5m",
+      executor: 'constant-arrival-rate',
+      duration: '10s',
       rate: rate,
-      timeUnit: "1s",
+      timeUnit: '1s',
       preAllocatedVUs: rate,
       maxVUs: rate * 5,
     },
   },
   thresholds: {
-    http_req_duration: [queryType === "Article" ? "p(99)<1000" : "p(99)<500"], // 99% of requests must complete below 1.5s
+    http_req_duration: [queryType === 'Article' ? 'p(99)<1000' : 'p(99)<500'], // 99% of requests must complete below 1.5s
   },
-};
+}
 
 const genPayload = (key) => {
-  const escaped = key.replace(/"/g, '\\"');
+  const escaped = key.replace(/"/g, '\\"')
   //console.log({key})
-  const queryUser = `{"operationName":"SearchAggregateUsersPublic","variables":{"key":"${escaped}"},"query":"query SearchAggregateUsersPublic($key: String!, $after: String, $version: SearchAPIVersion = ${queryVersion}) {\\n  search(input: {type: User, version: $version, first: 30, key: $key, after: $after}) {\\n    pageInfo {\\n      startCursor\\n      endCursor\\n      hasNextPage\\n      __typename\\n    }\\n    edges {\\n      cursor\\n      node {\\n        ... on User {\\n          ...UserDigestConciseUser\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment UserDigestConciseUser on User {\\n  id\\n  userName\\n  displayName\\n  status {\\n    state\\n    __typename\\n  }\\n  ...AvatarUser\\n  ...AvatarUserLogbook\\n  __typename\\n}\\n\\nfragment AvatarUser on User {\\n  avatar\\n  liker {\\n    civicLiker\\n    __typename\\n  }\\n  info {\\n    badges {\\n      type\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment AvatarUserLogbook on User {\\n  info {\\n    cryptoWallet {\\n      id\\n      address\\n      hasNFTs\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\\n"}`;
-  const queryTag = `{"operationName":"SearchAggregateTagsPublic","variables":{"key":"${escaped}"},"query":"query SearchAggregateTagsPublic($key: String!, $after: String, $version: SearchAPIVersion = ${queryVersion}) {\\n  search(input: {type: Tag, version: $version, first: 30, key: $key, after: $after}) {\\n    pageInfo {\\n      startCursor\\n      endCursor\\n      hasNextPage\\n      __typename\\n    }\\n    edges {\\n      cursor\\n      node {\\n        ... on Tag {\\n          ...TagDigestConciseTag\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment TagDigestConciseTag on Tag {\\n  id\\n  content\\n  numArticles\\n  __typename\\n}\\n"}`;
+  const queryUser = `{"operationName":"SearchAggregateUsersPublic","variables":{"key":"${escaped}"},"query":"query SearchAggregateUsersPublic($key: String!, $after: String, $version: SearchAPIVersion = ${queryVersion}) {\\n  search(input: {type: User, version: $version, first: 30, key: $key, after: $after}) {\\n    pageInfo {\\n      startCursor\\n      endCursor\\n      hasNextPage\\n      __typename\\n    }\\n    edges {\\n      cursor\\n      node {\\n        ... on User {\\n          ...UserDigestConciseUser\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment UserDigestConciseUser on User {\\n  id\\n  userName\\n  displayName\\n  status {\\n    state\\n    __typename\\n  }\\n  ...AvatarUser\\n  ...AvatarUserLogbook\\n  __typename\\n}\\n\\nfragment AvatarUser on User {\\n  avatar\\n  liker {\\n    civicLiker\\n    __typename\\n  }\\n  info {\\n    badges {\\n      type\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\\n\\nfragment AvatarUserLogbook on User {\\n  info {\\n    cryptoWallet {\\n      id\\n      address\\n      hasNFTs\\n      __typename\\n    }\\n    __typename\\n  }\\n  __typename\\n}\\n"}`
+  const queryTag = `{"operationName":"SearchAggregateTagsPublic","variables":{"key":"${escaped}"},"query":"query SearchAggregateTagsPublic($key: String!, $after: String, $version: SearchAPIVersion = ${queryVersion}) {\\n  search(input: {type: Tag, version: $version, first: 30, key: $key, after: $after}) {\\n    pageInfo {\\n      startCursor\\n      endCursor\\n      hasNextPage\\n      __typename\\n    }\\n    edges {\\n      cursor\\n      node {\\n        ... on Tag {\\n          ...TagDigestConciseTag\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\\nfragment TagDigestConciseTag on Tag {\\n  id\\n  content\\n  numArticles\\n  __typename\\n}\\n"}`
   const queryArticle = JSON.stringify({
-    operationName: "SearchAggregateArticlesPublic",
+    operationName: 'SearchAggregateArticlesPublic',
     persistedQuery: {
       version: 1,
       sha256Hash:
-        "9863f9e24556f746211b9f8c9bad61416b7c9dd393f6e8f3107185216d7a5b29",
+        '9863f9e24556f746211b9f8c9bad61416b7c9dd393f6e8f3107185216d7a5b29',
     },
     variables: { key },
     query: String.raw`### SearchAggregateArticlesPublic
@@ -380,31 +384,32 @@ fragment ArticleFeedFollowButtonUserPrivate on User {
   isFollowee
   __typename
 } `,
-  });
-  if (queryType === "User") {
-    return queryUser;
-  } else if (queryType === "Tag") {
-    return queryTag;
-  } else if (queryType == "Article") {
-    return queryArticle;
+  })
+  if (queryType === 'User') {
+    return queryUser
+  } else if (queryType === 'Tag') {
+    return queryTag
+  } else if (queryType == 'Article') {
+    return queryArticle
   } else {
-    throw queryType;
+    throw queryType
   }
-};
+}
 
-const splitLines = (str) => str.split(/\r?\n/);
-const keys = new SharedArray("search keys", () =>
-  splitLines(open("./matters-search_key-2022.txt"))
-);
+const splitLines = (str) => str.split(/\r?\n/)
+const keys = new SharedArray('search keys', () =>
+  //splitLines(open("./matters-search_key-2022.txt"))
+  splitLines(open('./matters-search_key-2022-small.txt'))
+)
 
 export default () => {
   const headers = {
-    "Content-Type": "application/json",
-  };
-  const key = keys[Math.floor(Math.random() * keys.length)];
-  const result = http.post(url, genPayload(key), { headers });
-  //console.log(result)
-  if (!check(result, { "status code is 200": (obj) => obj.status === 200 })) {
-    fail(JSON.stringify(result));
+    'Content-Type': 'application/json',
   }
-};
+  const key = keys[Math.floor(Math.random() * keys.length)]
+  const result = http.post(url, genPayload(key), { headers })
+  //console.log(result)
+  if (!check(result, { 'status code is 200': (obj) => obj.status === 200 })) {
+    fail(JSON.stringify(result))
+  }
+}
