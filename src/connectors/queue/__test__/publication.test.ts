2083ee8b460aa8d4e2557b8e1b2997145744fd02
xdj <xudongjie@laixi.pro>
2023-01-21 11:26:07 +0800

    test(publicationqueue): add publicationQueue test

--- src/connectors/queue/__test__/publication.test.ts
+++ src/connectors/queue/__test__/publication.test.ts
@@ -1,4 +1,6 @@
-import { PUBLISH_STATE } from 'common/enums'
+import { v4 } from 'uuid'
+
+import { ARTICLE_STATE, PUBLISH_STATE } from 'common/enums'
 import { publicationQueue } from 'connectors/queue'
 
 describe('publicationQueue.publishArticle', () => {
@@ -13,5 +15,58 @@ describe('publicationQueue.publishArticle', () => {
       draftId: notPendingDraftId,
     })
     await job.finished()
+    expect(await job.getState()).toBe('completed')
+  })
+  test('publish pending draft successfully', async () => {
+    const draft = await createPendingDraft()
+    const job = await publicationQueue.publishArticle({
+      draftId: draft.id,
+    })
+    await job.finished()
+    expect(await job.getState()).toBe('completed')
+    const updatedDraft = await publicationQueue.draftService.baseFindById(
+      draft.id
+    )
+    const updatedArticle = await publicationQueue.articleService.baseFindById(
+      updatedDraft.articleId
+    )
+    expect(updatedDraft.publishState).toBe(PUBLISH_STATE.published)
+    expect(updatedArticle.state).toBe(ARTICLE_STATE.active)
+  })
+  test('publish pending draft unsuccessfully', async () => {
+    // mock
+    publicationQueue.userService.baseFindById = async (id) => {
+      throw Error('mock error in publicationQueue test')
+    }
+    const draft = await createPendingDraft()
+    const job = await publicationQueue.publishArticle({
+      draftId: draft.id,
+    })
+    try {
+      await job.finished()
+    } catch {
+      // pass
+    }
+    expect(await job.getState()).toBe('failed')
+
+    const updatedDraft = await publicationQueue.draftService.baseFindById(
+      draft.id
+    )
+    const updatedArticle = await publicationQueue.articleService.baseFindById(
+      updatedDraft.articleId
+    )
+
+    expect(updatedDraft.publishState).toBe(PUBLISH_STATE.error)
+    expect(updatedArticle.state).toBe(ARTICLE_STATE.active)
   })
 })
+
+const createPendingDraft = async () =>
+  publicationQueue.draftService.baseCreate({
+    authorId: '1',
+    uuid: v4(),
+    title: 'test title',
+    summary: 'test summary',
+    content: 'test content',
+    publishState: PUBLISH_STATE.pending,
+  })
