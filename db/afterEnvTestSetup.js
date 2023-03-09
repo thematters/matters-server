fafee15a52c7c0a14e25b762abdb00527eedf5a0
xdj <xudongjie@laixi.pro>
2023-03-10 00:22:33 +0800

    move truncate back to `beforeAll`

--- db/afterEnvTestSetup.js
+++ db/afterEnvTestSetup.js
@@ -4,24 +4,6 @@ const knexConfig = require('../knexfile')
 const knex = Knex(knexConfig.test)
 
 beforeAll(async () => {
-  const { count } = await knex('public.user').count().first()
-  if (count === '0') {
-    await knex.seed.run()
-  }
-})
-
-afterAll(async () => {
-  // const getTables = async (knex) => {
-  //   const res = await knex.raw(
-  //     "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE'"
-  //   )
-  //   const tables = res.rows.map(({ table_name }) => table_name)
-  //   const dataTables = tables.filter(
-  //     (t) => !t.includes('knex') && t !== 'entity_type'
-  //   )
-  //   return dataTables.map((t) => 'public.' + t)
-  // }
-  // const tables = await getTables(knex)
   const tables = [
     'public.user',
     'verification_code',
@@ -64,4 +46,19 @@ afterAll(async () => {
     'report_asset',
   ]
   await knex.raw(`TRUNCATE ${tables.join(', ')} RESTART IDENTITY CASCADE;`)
+  await knex.seed.run()
 })
+
+// afterAll(async () => {
+//   const getTables = async (knex) => {
+//     const res = await knex.raw(
+//       "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_type = 'BASE TABLE'"
+//     )
+//     const tables = res.rows.map(({ table_name }) => table_name)
+//     const dataTables = tables.filter(
+//       (t) => !t.includes('knex') && t !== 'entity_type'
+//     )
+//     return dataTables.map((t) => 'public.' + t)
+//   }
+//   const tables = await getTables(knex)
+// })
