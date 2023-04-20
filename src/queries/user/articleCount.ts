82916a0ade2713e6cef1db65867e3123ee9c463c
xdj <xudongjie@laixi.pro>
2023-04-20 14:55:50 +0800

    revert: revert workaround in commit 31bde40e96fe

--- src/queries/user/articleCount.ts
+++ src/queries/user/articleCount.ts
@@ -4,23 +4,13 @@ import { UserStatusToArticleCountResolver } from 'definitions'
 const resolver: UserStatusToArticleCountResolver = async (
   { id },
   _,
-  { dataSources: { atomService }, knex }
+  { dataSources: { atomService } }
 ) => {
-  const result = await knex('article as a1')
-    .where({ 'a1.author_id': id })
-    .whereNotExists(
-      knex('article as a2')
-        .select(1)
-        .where('a2.draft_id', '=', knex.raw('a1.draft_id'))
-        .andWhere({
-          'a2.state': ARTICLE_STATE.archived,
-          'a2.author_id': id,
-        })
-    )
-    .count()
-    .first()
-
-  return parseInt(result ? (result.count as string) : '0', 10)
+  const count = await atomService.count({
+    table: 'article',
+    where: { authorId: id, state: ARTICLE_STATE.active },
+  })
+  return count
 }
 
 export default resolver
