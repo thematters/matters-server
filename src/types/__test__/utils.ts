0d41ba14e4f971c86316277e52b8a4dc5e9688a4
xdj <xudongjie@laixi.pro>
2023-04-26 18:30:18 +0800

    test(server): fix test apollo server

--- src/types/__test__/utils.ts
+++ src/types/__test__/utils.ts
@@ -118,7 +118,8 @@ export const testClient = async (
   const server = new ApolloServer({
     schema,
     context: ({ req }: { req: Request }) => {
-      return { req, ..._context, knex }
+      const newReq = req ? req : { headers: { Origin: 'https://matters.town' } }
+      return { req: newReq, ..._context, knex }
     },
     dataSources: () => ({
       atomService: new AtomService(),
