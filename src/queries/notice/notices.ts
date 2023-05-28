c430df35b9d01c9694ee87d59bcbce4d8eeac3f9
xdj <13580441+gary02@users.noreply.github.com>
2023-05-28 18:03:09 +0800

    feat(notice): add limit to notice resolve

--- src/queries/notice/notices.ts
+++ src/queries/notice/notices.ts
@@ -5,21 +5,31 @@ import {
 } from 'common/utils'
 import { UserToNoticesResolver } from 'definitions'
 
+const MAX_NOTICE_COUNT = 1000
+
 const resolver: UserToNoticesResolver = async (
   { id },
   { input },
   { dataSources: { notificationService } }
 ) => {
-  const { take, skip } = fromConnectionArgs(input)
+  const { take: _take, skip } = fromConnectionArgs(input)
+  let take = _take
+  if (_take + skip > MAX_NOTICE_COUNT) {
+    take = Math.max(MAX_NOTICE_COUNT - skip, 0)
+  }
 
-  const totalCount = await notificationService.notice.countNotice({
-    userId: id,
-  })
   const notices = await notificationService.notice.findByUser({
     userId: id,
+    onlyRecent: true,
     skip,
     take,
   })
+
+  const _totalCount = await notificationService.notice.countNotice({
+    userId: id,
+    onlyRecent: true,
+  })
+  const totalCount = Math.min(_totalCount, MAX_NOTICE_COUNT)
 
   const result = connectionFromArray(notices, input, totalCount)
   const edges = filterMissingFieldNoticeEdges(result.edges)
