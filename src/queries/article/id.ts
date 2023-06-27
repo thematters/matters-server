b9113e71166f5e87651029a467501eb48048a423
xdj <13580441+gary02@users.noreply.github.com>
2023-06-27 16:57:39 +0800

    refactor(article): move id resovler to single file

---
+++ src/queries/article/id.ts
@@ -0,0 +1,25 @@
+import type { ArticleToIdResolver } from 'definitions'
+
+import { NODE_TYPES } from 'common/enums'
+import { getLogger } from 'common/logger'
+import { toGlobalId } from 'common/utils'
+
+const logger = getLogger('query-article-id')
+
+const resolver: ArticleToIdResolver = async (
+  { articleId, id },
+  _,
+  __,
+  info
+) => {
+  if (!articleId) {
+    logger.warn(
+      "Article's fields should derive from Draft instead of Article: %j",
+      info.path
+    )
+    return toGlobalId({ type: NODE_TYPES.Article, id })
+  }
+  return toGlobalId({ type: NODE_TYPES.Article, id: articleId })
+}
+
+export default resolver
