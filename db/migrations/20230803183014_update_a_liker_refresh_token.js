b28a7ab77c4b9ea45b902fd9d522328ea91746c1
xdj <13580441+gary02@users.noreply.github.com>
2023-08-03 18:57:05 +0800

    feat: minor

--- db/migrations/20230803183014_update_a_liker_refresh_token.js
+++ db/migrations/20230803183014_update_a_liker_refresh_token.js
@@ -6,6 +6,6 @@ exports.up = async (knex) => {
 
 exports.down = async (knex) => {
   await knex.raw(
-    `update user_oauth_likecoin set refresh_token = 'test-token' where liker_id = 'kiutest2'`
+    `update user_oauth_likecoin set refresh_token = 'l6DLcy7GQsdatVGP7BAfK7_KBZxu9Yh-iPVn8CoBiGA' where liker_id = 'kiutest2'`
   )
 }
