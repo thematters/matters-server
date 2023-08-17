995c0f97db0d15b13251cf83ff1b78bd52c6a79f
robertu <4065233+robertu7@users.noreply.github.com>
2023-08-17 16:35:31 +0800

    refactor: temp disable `user.unreadFollowing`

--- src/queries/user/unreadFollowing.ts
+++ src/queries/user/unreadFollowing.ts
@@ -1,5 +1,5 @@
-import { ActivityType, LOG_RECORD_TYPES, MATERIALIZED_VIEW } from 'common/enums'
-import { readonlyKnex as knexRO } from 'connectors'
+// import { ActivityType, LOG_RECORD_TYPES, MATERIALIZED_VIEW } from 'common/enums'
+// import { readonlyKnex as knexRO } from 'connectors'
 import { UserStatusToUnreadFollowingResolver } from 'definitions'
 
 const resolver: UserStatusToUnreadFollowingResolver = async (
@@ -7,72 +7,74 @@ const resolver: UserStatusToUnreadFollowingResolver = async (
   _,
   { dataSources: { systemService } }
 ) => {
-  const readFollowingFeedLog = await systemService.findLogRecord({
-    userId,
-    type: LOG_RECORD_TYPES.ReadFollowingFeed,
-  })
+  return false
 
-  if (!readFollowingFeedLog) {
-    return true
-  }
+  // const readFollowingFeedLog = await systemService.findLogRecord({
+  //   userId,
+  //   type: LOG_RECORD_TYPES.ReadFollowingFeed,
+  // })
 
-  const latestActivity = await knexRO
-    .select()
-    .from(
-      // filter activies based on user's following user
-      knexRO
-        .as('selected_activities')
-        .select('acty.*')
-        .from('action_user as au')
-        .join(
-          `${MATERIALIZED_VIEW.user_activity_materialized} as acty`,
-          'acty.actor_id',
-          'au.target_id'
-        )
-        .where({
-          'au.user_id': userId,
-          'au.action': 'follow',
-          'acty.type': ActivityType.UserPublishArticleActivity,
-        })
-        .union([
-          // filter activities based on viewer's following tag
-          knexRO
-            .select('acty.*')
-            .from('action_tag as at')
-            .join(
-              `${MATERIALIZED_VIEW.user_activity_materialized} as acty`,
-              'acty.target_id',
-              'at.target_id'
-            )
-            .where({
-              'at.user_id': userId,
-              'at.action': 'follow',
-              'acty.type': ActivityType.UserAddArticleTagActivity,
-            }),
-          // filter activities based on viewer's following circle
-          knexRO
-            .select('acty.*')
-            .from('action_circle as ac')
-            .join(
-              `${MATERIALIZED_VIEW.user_activity_materialized} as acty`,
-              'acty.target_id',
-              'ac.target_id'
-            )
-            .where({
-              'ac.user_id': userId,
-              'ac.action': 'follow',
-              'acty.type': ActivityType.UserBroadcastCircleActivity,
-            }),
-        ])
-    )
-    .orderBy('created_at', 'desc')
-    .first()
+  // if (!readFollowingFeedLog) {
+  //   return true
+  // }
 
-  if (!latestActivity) {
-    return false
-  }
+  // const latestActivity = await knexRO
+  //   .select()
+  //   .from(
+  //     // filter activies based on user's following user
+  //     knexRO
+  //       .as('selected_activities')
+  //       .select('acty.*')
+  //       .from('action_user as au')
+  //       .join(
+  //         `${MATERIALIZED_VIEW.user_activity_materialized} as acty`,
+  //         'acty.actor_id',
+  //         'au.target_id'
+  //       )
+  //       .where({
+  //         'au.user_id': userId,
+  //         'au.action': 'follow',
+  //         'acty.type': ActivityType.UserPublishArticleActivity,
+  //       })
+  //       .union([
+  //         // filter activities based on viewer's following tag
+  //         knexRO
+  //           .select('acty.*')
+  //           .from('action_tag as at')
+  //           .join(
+  //             `${MATERIALIZED_VIEW.user_activity_materialized} as acty`,
+  //             'acty.target_id',
+  //             'at.target_id'
+  //           )
+  //           .where({
+  //             'at.user_id': userId,
+  //             'at.action': 'follow',
+  //             'acty.type': ActivityType.UserAddArticleTagActivity,
+  //           }),
+  //         // filter activities based on viewer's following circle
+  //         knexRO
+  //           .select('acty.*')
+  //           .from('action_circle as ac')
+  //           .join(
+  //             `${MATERIALIZED_VIEW.user_activity_materialized} as acty`,
+  //             'acty.target_id',
+  //             'ac.target_id'
+  //           )
+  //           .where({
+  //             'ac.user_id': userId,
+  //             'ac.action': 'follow',
+  //             'acty.type': ActivityType.UserBroadcastCircleActivity,
+  //           }),
+  //       ])
+  //   )
+  //   .orderBy('created_at', 'desc')
+  //   .first()
 
-  return readFollowingFeedLog.readAt < latestActivity.createdAt
+  // if (!latestActivity) {
+  //   return false
+  // }
+
+  // return readFollowingFeedLog.readAt < latestActivity.createdAt
 }
 
 export default resolver
