8cd539707cd2f33070c01858ea3bb7f7f8e44ac1
byhow <by.how@hotmail.com>
2023-10-10 00:34:15 -0700

    refactor: migrate contract interaction and log
    (cherry picked from commit 66b633f0d5be07e936cc17f1b0386739fd1f0e3a)

--- src/connectors/blockchain/curation.ts
+++ src/connectors/blockchain/curation.ts
@@ -1,6 +1,5 @@
-// import type { Log as EthersLog } from '@ethersproject/abstract-provider'
 import { Contract } from 'ethers'
-import { Log as EthersLog, decodeEventLog } from 'viem'
+import { Address, decodeEventLog } from 'viem'
 
 import { BLOCKCHAIN_CHAINID } from 'common/enums'
 import { environment, isProd } from 'common/environment'
@@ -32,16 +31,41 @@ export interface CurationTxReceipt {
 }
 
 // constants
-
-const erc20TokenCurationEventABI =
-  'event Curation(address indexed curator, address indexed creator, address indexed token, string uri, uint256 amount)'
+// erc20 token
 const erc20TokenCurationEventIdentifier =
   'Curation(address,address,address,string,uint256)'
-const nativeTokenCurationEventABI =
-  'event Curation(address indexed from, address indexed to, string uri, uint256 amount)'
+const erc20TokenCurationEventABI = [{
+  name: erc20TokenCurationEventIdentifier,
+  type: 'event',
+  inputs: [
+    { type: 'address', indexed: true, name: 'curator' },
+    { type: 'address', indexed: true, name: 'creator' },
+    { type: 'address', indexed: true, name: 'token' },
+    { type: 'string', name: 'uri' },
+    { type: 'uint256', name: 'amount' }
+  ]
+}] as const
+type Erc20Params = { from: `0x${string}`; to: `0x${string}`; uri: string; amount: bigint; }
+// native token
 const nativeTokenCurationEventIdentifier =
   'Curation(address,address,string,uint256)'
-const contractAddress = environment.polygonCurationContractAddress.toLowerCase()
+const nativeTokenCurationEventABI = [{
+  name: nativeTokenCurationEventIdentifier,
+  type: 'event',
+  inputs: [
+    { type: 'address', indexed: true, name: 'from' },
+    { type: 'address', indexed: true, name: 'to' },
+    { type: 'string', name: 'uri' },
+    { type: 'uint256', name: 'amount' }
+  ]
+}] as const
+type NativeTokenParams = { curator: `0x${string}`; creator: `0x${string}`; token: `0x${string}`; uri: string; amount: bigint; }
+// typeguards for viem
+const isErc20TokenParams = (t: Erc20Params | NativeTokenParams): t is Erc20Params => (t as Erc20Params).from !== undefined
+
+// additional constants
+const CURATION_ABI = [...erc20TokenCurationEventABI, ...nativeTokenCurationEventABI] as const
+const contractAddress = environment.polygonCurationContractAddress.toLowerCase() as Address
 
 const chainId = isProd
   ? BLOCKCHAIN_CHAINID.Polygon.PolygonMainnet
@@ -54,10 +78,7 @@ export class CurationContract extends BaseContract {
   public nativeTokenCurationEventTopic: string
 
   public constructor() {
-    super(parseInt(chainId, 10), contractAddress, [
-      erc20TokenCurationEventABI,
-      nativeTokenCurationEventABI,
-    ])
+    super(parseInt(chainId, 10), contractAddress, CURATION_ABI)
     this.erc20TokenCurationEventTopic = this.contract.interface.getEventTopic(
       erc20TokenCurationEventIdentifier
     )
@@ -99,32 +120,35 @@ export class CurationContract extends BaseContract {
   }
 
   public fetchTxReceipt = async (
-    txHash: string
+    txHash: `0x{string}`
   ): Promise<CurationTxReceipt | null> => {
-    const txReceipt = await this.provider.getTransactionReceipt(txHash)
+    const txReceipt = await this.client.getTransactionReceipt({ hash: txHash })
     if (!txReceipt) {
       return null
     }
-    const targets = txReceipt.logs.filter(
-      (log: EthersLog) =>
-        log.address.toLowerCase() === this.address.toLowerCase() &&
-        (log.topics[0] === this.erc20TokenCurationEventTopic ||
-          log.topics[0] === this.nativeTokenCurationEventTopic)
+    const targets = txReceipt.logs.filter(log =>
+      log.address.toLowerCase() === this.address.toLowerCase() &&
+      (log.topics[0] === this.erc20TokenCurationEventTopic ||
+        log.topics[0] === this.nativeTokenCurationEventTopic)
     )
-    const iface = this.contract.interface as unknown as Contract["interface"]
+
     return {
       txHash,
-      reverted: txReceipt.status === 0,
+      reverted: txReceipt.status === 'reverted',
       events: targets
-        // .map(log => iface.parseLog(log))
-        .map((log) => decodeEventLog(log))
-        .map((e) => ({
-          curatorAddress: (e.args!.curator! || e.args!.from!).toLowerCase(),
-          creatorAddress: (e.args!.creator! || e.args!.to!).toLowerCase(),
-          uri: e.args!.uri,
-          tokenAddress: e.args!.token! ? e.args!.token!.toLowerCase() : null,
-          amount: e.args!.amount!.toString(),
-        })),
+        .map((log) => decodeEventLog({ abi: this.contract.abi as typeof CURATION_ABI, ...log }))
+        .map((e) => {
+          const curatorAddress = (isErc20TokenParams(e.args) ? e.args.from : e.args.curator).toLowerCase()
+          const creatorAddress = (isErc20TokenParams(e.args) ? e.args.to : e.args.creator).toLowerCase()
+          const tokenAddress = !isErc20TokenParams(e.args) ? e.args?.token?.toLowerCase() : null
+          return {
+            curatorAddress,
+            creatorAddress,
+            uri: e.args?.uri,
+            tokenAddress,
+            amount: e.args?.amount?.toString(),
+          }
+        }),
     }
   }
 
