e690297e6eaa6db6c9a2c8e35f00d10a3b613e13
xdj <13580441+gary02@users.noreply.github.com>
2024-04-17 18:51:32 +0800

    fix(topic&chapter): migration failed as asset_map still ref topic in entity_type

--- db/migrations/20240326181309_drop_topic_chapter_related_tables.js
+++ db/migrations/20240326181309_drop_topic_chapter_related_tables.js
@@ -1,4 +1,21 @@
 exports.up = async (knex) => {
+  // delete data
+
+  const covers = await knex('topic').select('cover')
+  const assetIds = covers.map(({ cover }) => cover)
+
+  await knex('article_topic').del()
+  await knex('article_chapter').del()
+  await knex('chapter').del()
+  await knex('topic').del()
+
+  if (assetIds.length > 0) {
+    await knex('asset_map').whereIn('asset_id', assetIds).del()
+    await knex('asset').whereIn('id', assetIds).del()
+  }
+
+  // drop tables
+
   const deleteTable = async (table) => {
     await knex('entity_type')
       .where({
