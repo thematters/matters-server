e79f6f3594d6110b8b457ddcd91efaf8942e6fb9
xdj <13580441+gary02@users.noreply.github.com>
2024-05-14 15:17:13 +0800

    feat(draft): temporarily use article version as draft in article drafts API
    matters-web 4.30.2 (prod) still rely on those APIs for article revision, but server do not create
    new rafts but article-versions, we temporarily fake the APIs to fix the problem

--- src/queries/draft/article/drafts.ts
+++ src/queries/draft/article/drafts.ts
@@ -1,14 +1,32 @@
 import type { GQLArticleResolvers } from 'definitions'
 
 const resolver: GQLArticleResolvers['drafts'] = async (
-  { id: articleId },
+  { id: articleId, authorId },
   _,
   { dataSources: { atomService } }
-) =>
-  atomService.findMany({
-    table: 'draft',
+) => {
+  const versions = await atomService.findMany({
+    table: 'article_version',
     where: { articleId },
     orderBy: [{ column: 'created_at', order: 'desc' }],
   })
+  return versions.map(async (version) => {
+    return {
+      ...version,
+      authorId,
+      content: (
+        await atomService.articleContentIdLoader.load(version.contentId)
+      ).content,
+      publishState: 'published',
+      // unused fields in front-end
+      contentMd: '',
+      iscnPublish: false,
+      collection: null,
+      remark: null,
+      archived: false,
+      language: null,
+    }
+  })
+}
 
 export default resolver
