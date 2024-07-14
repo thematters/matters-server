83c71c4a987d0332a5adceb6678b8eab6d229ea8
j <13580441+gary02@users.noreply.github.com>
2024-07-14 17:50:33 +0800

    feat(notice): move `NoticeService` to lambda

--- src/connectors/notificationService/index.ts
+++ src/connectors/notificationService/index.ts
@@ -11,21 +11,18 @@ import type {
 import { MONTH, NOTICE_TYPE } from 'common/enums'
 
 import { mail } from './mail'
-import { Notice } from './notice'
 
 export class NotificationService {
   public mail: typeof mail
-  public notice: Notice
   private connections: Connections
 
   public constructor(connections: Connections) {
     this.connections = connections
     this.mail = mail
-    this.notice = new Notice(connections)
   }
 
   public trigger = async (params: NotificationParams) =>
-    this.notice.trigger(params)
+    console.log('trigger', params)
 
   public markAllNoticesAsRead = async (userId: string) => {
     const knex = this.connections.knex
