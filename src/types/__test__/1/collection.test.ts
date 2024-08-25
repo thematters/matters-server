2e5388df7387e3533872b650c247b10d57fcc245
j <13580441+gary02@users.noreply.github.com>
2024-08-25 17:04:38 +0800

    test(collection): more tests

--- src/types/__test__/1/collection.test.ts
+++ src/types/__test__/1/collection.test.ts
@@ -853,6 +853,56 @@ describe('get collection articles', () => {
     expect(data4?.node.articles.pageInfo.hasPreviousPage).toBe(false)
     expect(data4?.node.articles.pageInfo.hasNextPage).toBe(true)
     expect(data4?.node.articles.edges[0].node.id).toBe(articleGlobalIds[0])
+
+    // reversed by default
+    const { errors: errors5, data: data5 } = await server.executeOperation({
+      query: GET_COLLECTION_ARTICLES,
+      variables: {
+        collectionInput: { id: collectionGlobalId },
+        articleInput: { first: 2 },
+      },
+    })
+    expect(errors5).toBeUndefined()
+    expect(data5?.node.articles.totalCount).toBe(3)
+    expect(data5?.node.articles.edges.length).toBe(2)
+    expect(data5?.node.articles.pageInfo.hasPreviousPage).toBe(false)
+    expect(data5?.node.articles.pageInfo.hasNextPage).toBe(true)
+    expect(data5?.node.articles.edges[0].node.id).toBe(articleGlobalIds[2])
+    expect(data5?.node.articles.edges[1].node.id).toBe(articleGlobalIds[1])
+
+    // includeAfter
+    const { errors: errors6, data: data6 } = await server.executeOperation({
+      query: GET_COLLECTION_ARTICLES,
+      variables: {
+        collectionInput: { id: collectionGlobalId },
+        articleInput: {
+          first: 2,
+          after: data5?.node.articles.pageInfo.endCursor,
+          includeAfter: true,
+        },
+      },
+    })
+    expect(errors6).toBeUndefined()
+    expect(data6?.node.articles.totalCount).toBe(3)
+    expect(data6?.node.articles.edges.length).toBe(2)
+    expect(data6?.node.articles.pageInfo.hasPreviousPage).toBe(true)
+    expect(data6?.node.articles.pageInfo.hasNextPage).toBe(false)
+    expect(data6?.node.articles.edges[0].node.id).toBe(articleGlobalIds[1])
+    expect(data6?.node.articles.edges[1].node.id).toBe(articleGlobalIds[0])
+
+    // return total count if first is 0
+    const { errors: errors0, data: data0 } = await server.executeOperation({
+      query: GET_COLLECTION_ARTICLES,
+      variables: {
+        collectionInput: { id: collectionGlobalId },
+        articleInput: {
+          first: 0,
+        },
+      },
+    })
+
+    expect(errors0).toBeUndefined()
+    expect(data0?.node.articles.totalCount).toBe(3)
   })
 })
 
