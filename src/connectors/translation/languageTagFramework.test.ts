2fabdde5459d7aebd8dc388ee9e487be57083093
Li Zhineng <im@zhineng.li>
2024-09-06 17:24:41 +0800

    feat: language tag framework

---
+++ src/connectors/translation/languageTagFramework.test.ts
@@ -0,0 +1,85 @@
+import { Bcp47, Cldr } from './languageTagFramework'
+
+describe('BCP 47', () => {
+  it.each([
+    ['zh-Hans-CN', 'zh'],
+    ['Zh-Hans-CN', 'zh'],
+    ['ZH-Hans-CN', 'zh'],
+    ['zh', 'zh'],
+    ['en', 'en'],   // ISO 639-1
+    ['eng', 'eng'], // ISO 639-2/3
+  ])('parses language sub-tag %s', (tags: string, expected: string) => {
+    const bcp47 = new Bcp47(tags)
+    expect(bcp47.language()).toBe(expected)
+  })
+
+  it.each([
+    ['zh-Hans-CN', 'Hans'],
+    ['zh-Hans', 'Hans'],
+    ['zh-hans', 'Hans'],
+    ['zh-CN', null],
+    ['zh', null],
+  ])('parses script sub-tag %s', (tags: string, expected: string | null) => {
+    const bcp47 = new Bcp47(tags)
+    expect(bcp47.script()).toBe(expected)
+  })
+
+  it.each([
+    ['zh-Hans-CN', 'CN'],
+    ['zh-CN', 'CN'],
+    ['zh-cn', 'CN'],
+    ['zh-Hans', null],
+    ['zh', null],
+  ])('parses region sub-tag %s', (tags: string, expected: string | null) => {
+    const bcp47 = new Bcp47(tags)
+    expect(bcp47.region()).toBe(expected)
+  })
+
+  it('throws error when value is empty', () => {
+    expect(() => new Bcp47('')).toThrow('The value is empty.')
+    expect(() => new Bcp47(' ')).toThrow('The value is empty.')
+  })
+
+  it('throws error when language sub-tag is not iso 639-1 & 639-2/3', () => {
+    expect(() => new Bcp47('fooo'))
+      .toThrow('The language sub-tag does not conform to ISO 639-1 or 639-2/3.')
+  })
+
+  it.each([
+    ['zh-Hans-CN', 'zh-Hans-CN'],
+    ['zh-Hans', 'zh-Hans'],
+    ['zh-hans', 'zh-Hans'],
+    ['zh-CN', 'zh-CN'],
+    ['zh-cn', 'zh-CN'],
+    ['ZH', 'zh'],
+    ['en', 'en'],
+  ])('returns normalized value %s', (value: string, expected: string) => {
+    const bcp47 = new Bcp47(value)
+    expect(bcp47.value).toBe(expected)
+  })
+
+  it.each([
+    ['zh_CN', 'zh-CN'],
+    ['zh_Hans', 'zh-Hans'],
+    ['zh_Hans_CN', 'zh-Hans-CN'],
+    ['zh', 'zh'],
+  ])('creates an instance from cldr tags', (cldr: string, bcp47: string) => {
+    expect(Bcp47.from(new Cldr(cldr)).value).toBe(bcp47)
+  })
+})
+
+describe('CLDR', () => {
+  it('parses value by underscore seperator', () => {
+    const cldr = new Cldr('en_US')
+    expect(cldr.value).toBe('en_US')
+  })
+
+  it.each([
+    ['zh-CN', 'zh_CN'],
+    ['zh-Hans', 'zh_Hans'],
+    ['zh-Hans-CN', 'zh_Hans_CN'],
+    ['zh', 'zh'],
+  ])('creates an instance from bcp 47 tags %s', (bcp47: string, cldr: string) => {
+    expect(Cldr.from(new Bcp47(bcp47)).value).toBe(cldr)
+  })
+})
---
+++ src/connectors/translation/languageTagFramework.ts
@@ -0,0 +1,124 @@
+abstract class Framework {
+  #language: string = ''
+  #script: string | null = null
+  #region: string | null = null
+
+  constructor(value: string) {
+    this.#normalize(value.trim())
+  }
+
+  abstract get separator(): string
+
+  #normalize(value: string) {
+    if (value === '') {
+      throw new Error('The value is empty.')
+    }
+
+    const [language, script, region] = value.split(this.separator)
+    this.#language = this.#normalizeLanguage(language)
+
+    if (typeof script !== 'string') {
+      return
+    }
+
+    if (script.length === 4) {
+      this.#script = this.#normalizeScript(script)
+
+      if (typeof region === 'string') {
+        this.#region = this.#normalizeRegion(region)
+      }
+    } else if (script.length === 2) {
+      this.#region = this.#normalizeRegion(script)
+    }
+  }
+
+  #normalizeLanguage(language: string) {
+    if (language.length !== 2 && language.length !== 3) {
+      throw new Error(
+        'The language sub-tag does not conform to ISO 639-1 or 639-2/3.'
+      )
+    }
+
+    return language.toLowerCase()
+  }
+
+  #normalizeScript(script?: string) {
+    if (typeof script !== 'string') {
+      return null
+    }
+
+    if (script.length !== 4) {
+      return null
+    }
+
+    return script.charAt(0).toUpperCase() + script.slice(1).toLowerCase()
+  }
+
+  #normalizeRegion(region?: string) {
+    if (typeof region !== 'string') {
+      return null
+    }
+
+    if (region.length !== 2) {
+      return null
+    }
+
+    return region.toUpperCase()
+  }
+
+  language() {
+    return this.#language
+  }
+
+  script() {
+    return this.#script
+  }
+
+  region() {
+    return this.#region
+  }
+
+  get value() {
+    let value = this.#language
+
+    if (this.#script) {
+      value += `${this.separator}${this.#script}`
+    }
+
+    if (this.#region) {
+      value += `${this.separator}${this.#region}`
+    }
+
+    return value
+  }
+}
+
+/**
+ * Incomplete BCP 47 implementation.
+ *
+ * @link https://www.rfc-editor.org/rfc/bcp/bcp47.txt
+ */
+export class Bcp47 extends Framework {
+  static from(tags: Cldr) {
+    return new Bcp47(tags.value.replaceAll(tags.separator, '-'))
+  }
+
+  get separator(): string {
+    return '-'
+  }
+}
+
+/**
+ * Incomplete CLDR implementation.
+ *
+ * @link https://cldr.unicode.org
+ */
+export class Cldr extends Framework {
+  static from(tags: Bcp47) {
+    return new Cldr(tags.value.replaceAll(tags.separator, '_'))
+  }
+
+  get separator(): string {
+    return '_'
+  }
+}
