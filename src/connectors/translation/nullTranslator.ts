9a78c938e9a38d98d575a9abcc25be2a7f3d4117
Li Zhineng <im@zhineng.li>
2024-09-07 12:14:08 +0800

    feat: null translator with translate html

--- src/connectors/translation/nullTranslator.ts
+++ src/connectors/translation/nullTranslator.ts
@@ -1,11 +1,15 @@
-import { Translator } from './manager'
+import { HtmlTranslator, Translator } from './manager'
 
-export class NullTranslator implements Translator {
+export class NullTranslator implements Translator, HtmlTranslator {
   async detect(): Promise<string | null> {
     return 'en'
   }
 
   async translate(content: string): Promise<string | null> {
+    return content
+  }
+
+  async translateHtml(content: string): Promise<string | null> {
     return content
   }
 }
