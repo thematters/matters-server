59aea1d532371bfce96c352ad3cd21bb74f39198
Li Zhineng <im@zhineng.li>
2024-09-11 22:25:05 +0800

    docs: comment for human

--- src/initializers/configureClassification.ts
+++ src/initializers/configureClassification.ts
@@ -3,7 +3,27 @@ import { ClassificationManagerConfig, Manager } from 'connectors/classification/
 
 export function configureClassification() {
   const config: ClassificationManagerConfig = {
+
+    // --------------------------------------------------------------------
+    // Default Driver
+    // --------------------------------------------------------------------
+    //
+    // Specify the default classification driver to use when one is not
+    // specified in the "classifier" API. The driver must either be
+    // set in the drivers config, or added to the global manager.
+
     default: environment.classificationDefault || 'null',
+
+    // --------------------------------------------------------------------
+    // Classification Drivers
+    // --------------------------------------------------------------------
+    //
+    // Define a list of classification drivers that we can use in the
+    // application. The "classifier" API makes it simple to switch
+    // between different drivers to accommodate different cases.
+    //
+    // Supported drivers: "gemini" | "null"
+
     drivers: {
       gemini: {
         driver: 'gemini',
@@ -14,6 +34,7 @@ export function configureClassification() {
           keyFile: environment.classificationGeminiKeyFile,
         },
       },
+
       null: {
         driver: 'null',
       },
