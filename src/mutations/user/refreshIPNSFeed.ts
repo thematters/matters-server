84386e93a21dc1d45adbb4e2ec826259385dfd1c
gitwoz <177856586+gitwoz@users.noreply.github.com>
2024-11-06 13:27:58 +0800

    feat(ipns): revise `refreshIPNSFeed` mutation

--- src/mutations/user/refreshIPNSFeed.ts
+++ src/mutations/user/refreshIPNSFeed.ts
@@ -1,19 +1,20 @@
 import type { GQLMutationResolvers } from 'definitions'
 
+import { QUEUE_URL } from 'common/enums'
+import { aws } from 'connectors'
+
 const resolver: GQLMutationResolvers['refreshIPNSFeed'] = async (
   _,
   { input: { userName, numArticles = 50 } },
-  {
-    dataSources: {
-      userService,
-      queues: { publicationQueue },
-    },
-  }
+  { dataSources: { userService } }
 ) => {
   const ipnsKeyRec = await userService.findOrCreateIPNSKey(userName)
 
   if (ipnsKeyRec) {
-    // TODO
+    aws.sqsSendMessage({
+      messageBody: { userName, useMattersIPNS: true },
+      queueUrl: QUEUE_URL.ipnsUserPublication,
+    })
   }
 
   return userService.findByUserName(userName)
