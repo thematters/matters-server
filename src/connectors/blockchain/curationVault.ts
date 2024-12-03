618e12dbf26aaa52a12d60a8f55cba0fd4fd5dce
gitwoz <177856586+gitwoz@users.noreply.github.com>
2024-12-03 15:36:05 +0700

    feat(donation): support sync and confirm curation from vault contract

--- src/connectors/blockchain/curationVault.ts
+++ src/connectors/blockchain/curationVault.ts
@@ -14,12 +14,51 @@ import {
   parseSignature,
   keccak256,
   http,
+  Hex,
+  encodeEventTopics,
+  parseAbiItem,
+  decodeEventLog,
+  Hash,
 } from 'viem'
 
-import { BLOCKCHAIN_RPC, MINUTE } from 'common/enums'
+import {
+  BLOCKCHAIN,
+  BLOCKCHAIN_CHAINID,
+  BLOCKCHAIN_RPC,
+  MINUTE,
+} from 'common/enums'
 import { isProd } from 'common/environment'
 import { environment, contract } from 'common/environment'
 
+import { BaseContract } from './baseContract'
+
+// type
+export interface CurationVaultEvent {
+  curatorAddress: string
+  creatorId: string | null
+  uri: string
+  tokenAddress: string | null
+  amount: string
+}
+
+export interface Log<T> {
+  event: T
+  txHash: string
+  address: string
+  blockNumber: number
+  removed: boolean
+}
+
+export interface CurationVaultTxReceipt {
+  events: CurationVaultEvent[]
+  txHash: string
+  reverted: boolean
+  from: Address
+  to: Address | null
+  blockNumber: number
+}
+
+// constants
 const CURATION_VAULT_ABI = [
   {
     anonymous: false,
@@ -127,21 +166,184 @@ const CURATION_VAULT_ABI = [
   },
 ] as const
 
-export class CurationVaultContract {
-  public address: Address
+const erc20TokenCurationEventSignature =
+  'event Curation(address indexed curator, string indexed uid, address indexed token, string uri, uint256 amount)' as const
+const erc20TokenCurationEventABI = [
+  {
+    name: 'Curation',
+    type: 'event',
+    inputs: [
+      { type: 'address', indexed: true, name: 'curator' },
+      { type: 'string', indexed: true, name: 'uid' },
+      { type: 'address', indexed: true, name: 'token' },
+      { type: 'string', name: 'uri' },
+      { type: 'uint256', name: 'amount' },
+    ],
+  },
+] as const
+const nativeTokenCurationEventSignature =
+  'event Curation(address indexed from, string indexed uid, string uri, uint256 amount)' as const
+const nativeTokenCurationEventABI = [
+  {
+    name: 'Curation',
+    type: 'event',
+    inputs: [
+      { type: 'address', indexed: true, name: 'from' },
+      { type: 'string', indexed: true, name: 'uid' },
+      { type: 'string', name: 'uri' },
+      { type: 'uint256', name: 'amount' },
+    ],
+  },
+] as const
+
+const CURATION_EVENT_ABI = [
+  ...erc20TokenCurationEventABI,
+  ...nativeTokenCurationEventABI,
+] as const
+
+export class CurationVaultContract extends BaseContract {
   public signer: SmartAccountSigner
 
+  public erc20TokenCurationEventTopic: Hex[]
+  public nativeTokenCurationEventTopic: Hex[]
+
   public constructor() {
-    this.address = contract.Optimism.curationVaultAddress as `0x${string}`
+    // support OP only
+    const chainId = BLOCKCHAIN_CHAINID[BLOCKCHAIN.Optimism]
+    const contractAddress = contract.Optimism.curationVaultAddress as Address
+
+    super(chainId, contractAddress, contract.Optimism.curationVaultBlockNum)
+
     this.signer = LocalAccountSigner.privateKeyToAccountSigner(
       environment.curationVaultSignerPrivateKey as `0x${string}`
     )
+    this.erc20TokenCurationEventTopic = encodeEventTopics({
+      abi: erc20TokenCurationEventABI,
+      eventName: 'Curation',
+    }) as Hex[]
+    this.nativeTokenCurationEventTopic = encodeEventTopics({
+      abi: nativeTokenCurationEventABI,
+      eventName: 'Curation',
+    }) as Hex[]
+  }
+
+  public fetchLogs = async (
+    fromBlock: bigint,
+    toBlock: bigint
+  ): Promise<Array<Log<CurationVaultEvent>>> => {
+    const [erc20Logs, nativeLogs] = await Promise.all([
+      this.client.getFilterLogs({
+        filter: await this.client.createEventFilter({
+          event: parseAbiItem(erc20TokenCurationEventSignature),
+          fromBlock: BigInt(fromBlock),
+          toBlock: BigInt(toBlock),
+        }),
+      }),
+      this.client.getFilterLogs({
+        filter: await this.client.createEventFilter({
+          event: parseAbiItem(nativeTokenCurationEventSignature),
+          fromBlock: BigInt(fromBlock),
+          toBlock: BigInt(toBlock),
+        }),
+      }),
+    ])
+
+    // parse erc20logs and add result to logs
+    const logs: Array<Log<CurationVaultEvent>> = []
+    ;[...erc20Logs, ...nativeLogs].forEach((log) => {
+      const { args: logArgs } = decodeEventLog({
+        abi: CURATION_EVENT_ABI,
+        data: log.data,
+        topics: log.topics as [signature: `0x${string}`, ...hex: Hex[]],
+      })
+      const baseLog = {
+        txHash: log.transactionHash,
+        address: this.address,
+        blockNumber: Number(log.blockNumber),
+        removed: log.removed,
+      }
+
+      // ERC20
+      if ('curator' in logArgs) {
+        logs.push({
+          event: {
+            curatorAddress: logArgs.curator.toLowerCase(),
+            creatorId: this.parseUID(logArgs.uid),
+            uri: logArgs.uri,
+            tokenAddress: logArgs.token.toLowerCase(),
+            amount: logArgs.amount.toString(),
+          },
+          ...baseLog,
+        })
+      }
+      // native
+      else {
+        logs.push({
+          event: {
+            curatorAddress: logArgs.from.toLowerCase(),
+            creatorId: this.parseUID(logArgs.uid),
+            uri: logArgs.uri,
+            tokenAddress: null,
+            amount: logArgs.amount.toString(),
+          },
+          ...baseLog,
+        })
+      }
+    })
+
+    return logs
+  }
+
+  public fetchTxReceipt = async (
+    txHash: Hash
+  ): Promise<CurationVaultTxReceipt | null> => {
+    const txReceipt = await this.client.getTransactionReceipt({ hash: txHash })
+    if (!txReceipt) {
+      return null
+    }
+    const targets = txReceipt.logs.filter(
+      (log) =>
+        log.address.toLowerCase() === this.address.toLowerCase() &&
+        (log.topics[0] === this.erc20TokenCurationEventTopic[0] ||
+          log.topics[0] === this.nativeTokenCurationEventTopic[0])
+    )
+
+    return {
+      txHash,
+      reverted: txReceipt.status === 'reverted',
+      from: txReceipt.from,
+      to: txReceipt.to,
+      blockNumber: Number(txReceipt.blockNumber),
+      events: targets
+        .map((log) =>
+          decodeEventLog({
+            abi: CURATION_EVENT_ABI,
+            data: log.data,
+            topics: log.topics as [signature: `0x${string}`, ...hex: Hex[]],
+          })
+        )
+        .map((e) => {
+          const log = e.args
+          const isERC20 = 'curator' in log
+          const curatorAddress = (
+            isERC20 ? log.curator : log.from
+          ).toLowerCase()
+          const tokenAddress = isERC20 ? log.token.toLowerCase() : null
+          const uid = this.parseUID(log.uid)
+
+          return {
+            curatorAddress,
+            creatorId: uid,
+            uri: log?.uri,
+            tokenAddress,
+            amount: log?.amount?.toString(),
+          }
+        }),
+    }
   }
 
   public async getClient() {
-    const rpcTransport = http(
-      BLOCKCHAIN_RPC[isProd ? optimism.id : optimismSepolia.id]
-    ) as HttpTransport
+    const rpcTransport = http(BLOCKCHAIN_RPC[this.chainId]) as HttpTransport
 
     return createSmartAccountClient({
       transport: rpcTransport,
@@ -159,10 +361,11 @@ export class CurationVaultContract {
   }
 
   public parseUID(uid: string) {
-    if (!uid.startsWith('matters:')) {
-      throw new Error('Invalid UID')
+    const match = uid.match(/^matters:(\d+)$/)
+    if (!match) {
+      return null
     }
-    return uid.split(':')[1]
+    return match[1]
   }
 
   public async getWithdrawableUSDTAmount(userId: string) {
