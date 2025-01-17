2660039ba79de85427e450e0ec02cd98cc9ed329
gitwoz <177856586+gitwoz@users.noreply.github.com>
2025-01-17 13:57:13 +0700

    feat(channel): correct article id to classify channel

--- src/mutations/channel/classifyArticlesChannels.ts
+++ src/mutations/channel/classifyArticlesChannels.ts
@@ -1,5 +1,6 @@
 import type { GQLMutationResolvers } from 'definitions'
 
+import { ARTICLE_CHANNEL_JOB_STATE } from 'common/enums'
 import { AuthenticationError } from 'common/errors'
 import { fromGlobalId } from 'common/utils'
 
@@ -20,7 +21,7 @@ const resolver: GQLMutationResolvers['classifyArticlesChannels'] = async (
     table: 'article',
     whereIn: ['id', articleIds],
   })
-  const filteredArticleIds = articles
+  const nonSpamArticleIds = articles
     .filter((article) => {
       if (article.isSpam) {
         return false
@@ -37,6 +38,21 @@ const resolver: GQLMutationResolvers['classifyArticlesChannels'] = async (
       return true
     })
     .map((article) => article.id)
+
+  // filter articles that already have channel classification
+  const articleChannelJobs = await atomService.findMany({
+    table: 'article_channel_job',
+    whereIn: ['articleId', nonSpamArticleIds],
+  })
+  const filteredArticleIds = nonSpamArticleIds.filter(
+    (articleId) =>
+      !articleChannelJobs.some(
+        (job) =>
+          job.articleId === articleId &&
+          (job.state === ARTICLE_CHANNEL_JOB_STATE.finished ||
+            job.state === ARTICLE_CHANNEL_JOB_STATE.processing)
+      )
+  )
 
   await channelService.classifyArticlesChannels({ ids: filteredArticleIds })
 
