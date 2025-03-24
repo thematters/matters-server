f47806e37449b14053cbed21f5e09a353c33e3f1
gitwoz <177856586+gitwoz@users.noreply.github.com>
2025-03-24 15:18:43 +0700

    feat(channel): remove admin auth requirement for enabled field

--- src/types/channel.ts
+++ src/types/channel.ts
@@ -13,7 +13,7 @@ export default /* GraphQL */ `
 
     description(input: TranslationArgs): String
     providerId: String! @auth(mode: "${AUTH_MODE.admin}")
-    enabled: Boolean! @auth(mode: "${AUTH_MODE.admin}")
+    enabled: Boolean!
 
     articles(input: ConnectionArgs!): ArticleConnection! @complexity(multipliers: ["input.first"], value: 1)
   }
@@ -27,6 +27,7 @@ export default /* GraphQL */ `
   input ChannelInput {
     shortHash: String!
   }
+
 
   input PutChannelInput {
     id: ID
