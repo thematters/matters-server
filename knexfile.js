1262622b37bbbeea7cd05a372922379552eb2639
T-bag <noreply@github.com>
2025-04-09 15:54:04 +0000

    chore(knexfile): add acquireConnectionTimeout to database pool configuration

--- knexfile.js
+++ knexfile.js
@@ -35,7 +35,8 @@ export default {
     ...baseConfig,
     // set pool size to 1 to detect db connection acquiring deadlock
     // explained in https://github.com/Vincit/objection.js/issues/1137#issuecomment-561149456
-    pool: { min: 1, max: 1 },
+    pool: { min: 1, max: 1},
+    acquireConnectionTimeout: 60000 * 2,
   },
 
   development: baseConfig,
