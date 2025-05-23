5304cc69b77a32bf28554f2311ab4d64f719cd7d
T-bag <noreply@github.com>
2025-05-23 09:50:15 +0000

    docs(db): add comments

--- db/migrations/20250523161118_add_index_to_user_reader_materialized.js
+++ db/migrations/20250523161118_add_index_to_user_reader_materialized.js
@@ -2,10 +2,12 @@ const table = 'user_reader_materialized'
 const indexName = 'user_reader_materialized_author_score_id_idx'
 
 export const up = async (knex) => {
+  // update index to use author_score DESC NULLS LAST
   await knex.schema.table(table, (t) => {
     t.dropIndex(['author_score', 'id'])
     t.index([knex.raw('author_score DESC NULLS LAST'), 'id'], indexName)
   })
+  // remove redundant index: "user_reader_materialized_author_score" btree (author_score)
   await knex.raw(`DROP INDEX IF EXISTS user_reader_materialized_author_score`)
 }
 
