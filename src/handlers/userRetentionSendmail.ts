b648589e48f6fce90cdc9a720e0a036f84d92aea
Raven <noreply@github.com>
2025-06-10 08:41:11 +0000

    fix(user-retention): update sendmail parameters to ensure lastSeen is a Date object

--- src/handlers/userRetentionSendmail.ts
+++ src/handlers/userRetentionSendmail.ts
@@ -7,15 +7,15 @@ import { connections } from '../connections.js'
 const userRetentionService = new UserRetentionService(connections)
 
 export const handler = async (event: SQSEvent) => {
-  console.log(event)
+  console.log(event.Records)
   const results = await Promise.allSettled(
     event.Records.map(async ({ body }: { body: string }) => {
       const { userId, lastSeen, type } = JSON.parse(body)
-      await userRetentionService.sendmail(userId, lastSeen, type)
+      await userRetentionService.sendmail(userId, new Date(lastSeen), type)
     })
   )
 
-  // print failed reseaon
+  // print failed reason
   results.map((res) => {
     if (res.status === 'rejected') {
       console.error('sendmail failed, reason:')
