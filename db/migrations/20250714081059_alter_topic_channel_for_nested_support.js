51f168d9ca7b4c212ad8e0d6fe2aaf5472ad1c22
Rick <noreply@github.com>
2025-07-14 22:33:30 +0800

    feat(channel): alter `topic_channel` schema to support nested channels

---
+++ db/migrations/20250714081059_alter_topic_channel_for_nested_support.js
@@ -0,0 +1,17 @@
+const table = 'topic_channel'
+
+export const up = async (knex) => {
+  await knex.schema.table(table, (t) => {
+    t.specificType('pinned_articles', 'bigint ARRAY')
+    t.string('provider_id').nullable().alter()
+    t.integer('parent_id').nullable().references('id').inTable('topic_channel')
+  })
+}
+
+export const down = async (knex) => {
+  await knex.schema.table(table, (t) => {
+    t.dropColumn('pinned_articles')
+    t.string('provider_id').notNullable().alter()
+    t.dropColumn('parent_id')
+  })
+}
