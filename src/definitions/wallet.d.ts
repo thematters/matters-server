thematters/matters-server

commit 638c5389ece151ab536922384b5af865238e1894

feat(donation): add donator union type and refactor CryptoWallet related types

src/definitions/wallet.d.ts
@@ -1,5 +1,12 @@
 import type { GQLSigningMessagePurpose } from './schema.js'
 
+// used in `codegen.json`, not db type
+export interface Wallet {
+  address: string
+  id?: string
+  userId?: string
+}
+
 export interface CryptoWallet {
   id: string
   userId: string | null

src/queries/user/cryptoWallet.ts
@@ -1,10 +1,14 @@
 import type { GQLUserInfoResolvers } from '#definitions/index.js'
 
 const resolver: GQLUserInfoResolvers['cryptoWallet'] = async (
-  { id },
+  { id, ethAddress },
   _,
   { dataSources: { userService, atomService } }
 ) => {
+  if (ethAddress) {
+    return { address: ethAddress }
+  }
+
   if (id === undefined) {
     return null
   }
@@ -21,7 +25,7 @@ const resolver: GQLUserInfoResolvers['cryptoWallet'] = async (
   })
 
   if (wallet) {
-    // userId to override id
+    // user.id to override wallet.id
     return { id, userId: id, address: wallet.address }
   }
 

src/queries/user/index.ts
@@ -1,5 +1,6 @@
 import type {
   GQLAppreciationResolvers,
+  GQLDonatorResolvers,
   GQLCryptoWalletResolvers,
   GQLFollowingActivityResolvers,
   GQLFollowingResolvers,
@@ -86,6 +87,8 @@ const user: {
   UserActivity: GQLUserActivityResolvers
   UserAnalytics: GQLUserAnalyticsResolvers
   UserStatus: GQLUserStatusResolvers
+  Donator: GQLDonatorResolvers
+
   Appreciation: GQLAppreciationResolvers
 
   Following: GQLFollowingResolvers
@@ -176,6 +179,10 @@ const user: {
     changeEmailTimesLeft,
     hasEmailLoginPassword: ({ passwordHash }) => passwordHash !== null,
   },
+  Donator: {
+    __resolveType: ({ __type }: any) => __type,
+  },
+
   Appreciation,
 
   Following,

src/queries/user/nfts.ts
@@ -25,6 +25,9 @@ export const hasNFTs: GQLCryptoWalletResolvers['hasNFTs'] = async (
     },
   }
 ) => {
+  if (!userId) {
+    return false
+  }
   const cache = new Cache(CACHE_PREFIX.NFTS, objectCacheRedis)
 
   const user = await userService.baseFindById(userId)
@@ -57,6 +60,10 @@ export const nfts: GQLCryptoWalletResolvers['nfts'] = async (
     },
   }
 ) => {
+  if (!userId) {
+    return []
+  }
+
   const cache = new Cache(CACHE_PREFIX.NFTS, objectCacheRedis)
 
   const user = await userService.baseFindById(userId)

schema.graphql
@@ -2842,9 +2842,11 @@ type TopDonatorConnection implements Connection {
   edges: [TopDonatorEdge!]
 }
 
+union Donator = User | CryptoWallet
+
 type TopDonatorEdge {
   cursor: String!
-  node: User!
+  node: Donator!
   donationCount: Int!
 }
 

src/definitions/schema.d.ts
@@ -5,9 +5,9 @@ import {
 } from 'graphql'
 import {
   User as UserModel,
-  Wallet as WalletModel,
   OAuthClientDB as OAuthClientDBModel,
 } from './user.js'
+import { Wallet as WalletModel } from './wallet.js'
 import { Tag as TagModel } from './tag.js'
 import { Collection as CollectionModel } from './collection.js'
 import { Comment as CommentModel } from './comment.js'
@@ -1540,6 +1540,8 @@ export type GQLDirectImageUploadInput = {
   type: GQLAssetType
   url?: InputMaybe<Scalars['String']['input']>
 }
+
+export type GQLDonator = GQLCryptoWallet | GQLUser
 
 /** This type contains content, collections, assets and related data of a draft. */
 export type GQLDraft = GQLNode & {
@@ -3865,7 +3867,7 @@ export type GQLTopDonatorEdge = {
   __typename?: 'TopDonatorEdge'
   cursor: Scalars['String']['output']
   donationCount: Scalars['Int']['output']
-  node: GQLUser
+  node: GQLDonator
 }
 
 export type GQLTopDonatorFilter = {
@@ -4782,6 +4784,7 @@ export type DirectiveResolverFn<
 /** Mapping of union types */
 export type GQLResolversUnionTypes<_RefType extends Record<string, unknown>> =
   ResolversObject<{
+    Donator: WalletModel | UserModel
     FollowingActivity:
       | (Omit<GQLArticleRecommendationActivity, 'nodes'> & {
           nodes?: Maybe<Array<_RefType['Article']>>
@@ -5206,6 +5209,9 @@ export type GQLResolversTypes = ResolversObject<{
   DeleteMomentInput: GQLDeleteMomentInput
   DeleteTagsInput: GQLDeleteTagsInput
   DirectImageUploadInput: GQLDirectImageUploadInput
+  Donator: ResolverTypeWrapper<
+    GQLResolversUnionTypes<GQLResolversTypes>['Donator']
+  >
   Draft: ResolverTypeWrapper<DraftModel>
   DraftAccess: ResolverTypeWrapper<DraftModel>
   DraftConnection: ResolverTypeWrapper<
@@ -5536,7 +5542,7 @@ export type GQLResolversTypes = ResolversObject<{
     }
   >
   TopDonatorEdge: ResolverTypeWrapper<
-    Omit<GQLTopDonatorEdge, 'node'> & { node: GQLResolversTypes['User'] }
+    Omit<GQLTopDonatorEdge, 'node'> & { node: GQLResolversTypes['Donator'] }
   >
   TopDonatorFilter: GQLTopDonatorFilter
   TopDonatorInput: GQLTopDonatorInput
@@ -5886,6 +5892,7 @@ export type GQLResolversParentTypes = ResolversObject<{
   DeleteMomentInput: GQLDeleteMomentInput
   DeleteTagsInput: GQLDeleteTagsInput
   DirectImageUploadInput: GQLDirectImageUploadInput
+  Donator: GQLResolversUnionTypes<GQLResolversParentTypes>['Donator']
   Draft: DraftModel
   DraftAccess: DraftModel
   DraftConnection: Omit<GQLDraftConnection, 'edges'> & {
@@ -6133,7 +6140,7 @@ export type GQLResolversParentTypes = ResolversObject<{
     edges?: Maybe<Array<GQLResolversParentTypes['TopDonatorEdge']>>
   }
   TopDonatorEdge: Omit<GQLTopDonatorEdge, 'node'> & {
-    node: GQLResolversParentTypes['User']
+    node: GQLResolversParentTypes['Donator']
   }
   TopDonatorFilter: GQLTopDonatorFilter
   TopDonatorInput: GQLTopDonatorInput
@@ -7840,6 +7847,13 @@ export type GQLDatetimeRangeResolvers<
   end?: Resolver<Maybe<GQLResolversTypes['DateTime']>, ParentType, ContextType>
   start?: Resolver<GQLResolversTypes['DateTime'], ParentType, ContextType>
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>
+}>
+
+export type GQLDonatorResolvers<
+  ContextType = Context,
+  ParentType extends GQLResolversParentTypes['Donator'] = GQLResolversParentTypes['Donator']
+> = ResolversObject<{
+  __resolveType: TypeResolveFn<'CryptoWallet' | 'User', ParentType, ContextType>
 }>
 
 export type GQLDraftResolvers<
@@ -9927,7 +9941,7 @@ export type GQLTopDonatorEdgeResolvers<
 > = ResolversObject<{
   cursor?: Resolver<GQLResolversTypes['String'], ParentType, ContextType>
   donationCount?: Resolver<GQLResolversTypes['Int'], ParentType, ContextType>
-  node?: Resolver<GQLResolversTypes['User'], ParentType, ContextType>
+  node?: Resolver<GQLResolversTypes['Donator'], ParentType, ContextType>
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>
 }>
 
@@ -10886,6 +10900,7 @@ export type GQLResolvers<ContextType = Context> = ResolversObject<{
   CurationChannel?: GQLCurationChannelResolvers<ContextType>
   DateTime?: GraphQLScalarType
   DatetimeRange?: GQLDatetimeRangeResolvers<ContextType>
+  Donator?: GQLDonatorResolvers<ContextType>
   Draft?: GQLDraftResolvers<ContextType>
   DraftAccess?: GQLDraftAccessResolvers<ContextType>
   DraftConnection?: GQLDraftConnectionResolvers<ContextType>

src/definitions/user.d.ts
@@ -76,12 +76,6 @@ export type Viewer = (User & ViewerBase) | ViewerBase
 
 export type AuthMode = 'visitor' | 'oauth' | 'user' | 'admin'
 
-export interface Wallet {
-  id: string
-  userId: string
-  address: string
-}
-
 export interface SocialAccount {
   userId: string
   type: keyof typeof SOCIAL_LOGIN_TYPE

src/types/user.ts
@@ -360,9 +360,11 @@ export default /* GraphQL */ `
     edges: [TopDonatorEdge!]
   }
 
+  union Donator = User | CryptoWallet
+
   type TopDonatorEdge {
     cursor: String!
-    node: User!
+    node: Donator!
     donationCount: Int!
   }
 

commit 32b6cb64f4b6fb4c29874db3760bad4806df2970

fix(wallet): refactor and fix cryptoWallet resolver to always return userId as the cache key for NFTs.
The previous commit 638c5389ece151ab536922384b5af865238e1894 introduced
a bug in the cryptoWallet resolver, which returned only the address but
omitted the userId, which is required as the cache key in NFT queries.

src/definitions/wallet.d.ts
@@ -3,7 +3,6 @@ import type { GQLSigningMessagePurpose } from './schema.js'
 // used in `codegen.json`, not db type
 export interface Wallet {
   address: string
-  id?: string
   userId?: string
 }
 

src/queries/user/analytics/topDonators.ts
@@ -1,9 +1,6 @@
 import type { GQLUserAnalyticsResolvers } from '#definitions/index.js'
 
-import {
-  connectionFromArray,
-  connectionFromQueryOffsetBased,
-} from '#common/utils/index.js'
+import { connectionFromQueryOffsetBased } from '#common/utils/index.js'
 
 const resolver: GQLUserAnalyticsResolvers['topDonators'] = async (
   { id },
@@ -11,7 +8,14 @@ const resolver: GQLUserAnalyticsResolvers['topDonators'] = async (
   { dataSources: { userService, atomService } }
 ) => {
   if (!id) {
-    return connectionFromArray([], input)
+    return {
+      edges: [],
+      pageInfo: {
+        hasPreviousPage: false,
+        hasNextPage: false,
+      },
+      totalCount: 0,
+    }
   }
   const range = {
     start: input?.filter?.inRangeStart,
@@ -42,7 +46,7 @@ const resolver: GQLUserAnalyticsResolvers['topDonators'] = async (
         : { __type: 'CryptoWallet', address: edge.node.address },
       donationCount: edge.node.donationCount,
     })),
-  } as any
+  }
 }
 
 export default resolver

src/queries/user/cryptoWallet.ts
@@ -3,33 +3,21 @@ import type { GQLUserInfoResolvers } from '#definitions/index.js'
 const resolver: GQLUserInfoResolvers['cryptoWallet'] = async (
   { id, ethAddress },
   _,
-  { dataSources: { userService, atomService } }
+  { dataSources: { atomService } }
 ) => {
   if (ethAddress) {
-    return { address: ethAddress }
+    return { userId: id, address: ethAddress }
   }
-
-  if (id === undefined) {
-    return null
-  }
-
-  const user = await userService.baseFindById(id)
-  if (user.ethAddress) {
-    // fake a crypto_wallet, use userId as hasNFTs cache layer
-    return { id, userId: id, address: user.ethAddress }
-  }
-
   const wallet = await atomService.findFirst({
     table: 'crypto_wallet',
     where: { userId: id, archived: false },
   })
 
   if (wallet) {
-    // user.id to override wallet.id
-    return { id, userId: id, address: wallet.address }
+    return { userId: id, address: wallet.address }
   }
 
-  return wallet
+  return { userId: id, address: '' }
 }
 
 export default resolver

src/queries/user/index.ts
@@ -214,11 +214,9 @@ const user: {
   StripeAccount,
 
   CryptoWallet: {
-    id: ({ id }) =>
-      id ? toGlobalId({ type: NODE_TYPES.CryptoWallet, id }) : ('' as GlobalId),
+    id: ({ userId }) =>
+      toGlobalId({ type: NODE_TYPES.CryptoWallet, id: userId ?? '' }),
     address: ({ address }) => address,
-    // createdAt: ({ createdAt }) => createdAt,
-
     hasNFTs,
     nfts,
   },

src/queries/user/nfts.ts
@@ -20,24 +20,20 @@ export const hasNFTs: GQLCryptoWalletResolvers['hasNFTs'] = async (
   _,
   {
     dataSources: {
-      atomService,
       connections: { objectCacheRedis },
     },
   }
 ) => {
   const cache = new Cache(CACHE_PREFIX.NFTS, objectCacheRedis)
 
-  const owner = userId
-    ? (await atomService.userIdLoader.load(userId))?.ethAddress || address
-    : address
   const withMetadata = true
 
   const network = AlchemyNetwork.Mainnet
   const assets = (await cache.getObject({
-    keys: { type: 'traveloggers', id: owner },
+    keys: { type: 'traveloggers', id: userId },
     getter: () =>
       alchemy.getNFTs({
-        owner,
+        owner: address,
         contract: contract.Ethereum.traveloggersAddress,
         network,
         withMetadata,
@@ -53,24 +49,20 @@ export const nfts: GQLCryptoWalletResolvers['nfts'] = async (
   _,
   {
     dataSources: {
-      atomService,
       connections: { objectCacheRedis },
     },
   }
 ) => {
   const cache = new Cache(CACHE_PREFIX.NFTS, objectCacheRedis)
 
-  const owner = userId
-    ? (await atomService.userIdLoader.load(userId))?.ethAddress || address
-    : address
   const network = AlchemyNetwork.Mainnet
   const withMetadata = true
 
   const assets = (await cache.getObject({
-    keys: { type: 'traveloggers', id: owner },
+    keys: { type: 'traveloggers', id: userId },
     getter: () =>
       alchemy.getNFTs({
-        owner,
+        owner: address,
         network,
         contract: contract.Ethereum.traveloggersAddress,
         withMetadata,