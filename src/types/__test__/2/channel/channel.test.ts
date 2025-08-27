7d16a4de591959c05f0976a347073d51114a6d64
Jerry <norepy@github.com>
2025-08-26 21:32:56 -0400

    test(tag): correct tests

--- src/types/__test__/2/channel/channel.test.ts
+++ src/types/__test__/2/channel/channel.test.ts
@@ -259,7 +259,7 @@ describe('channel query', () => {
       toGlobalId({ type: NODE_TYPES.Tag, id: tag.id })
     )
     expect(data.channel.shortHash).toBe(tag.shortHash)
-    expect(data.channel.content).toBe('tag-channel')
+    expect(data.channel.content).toBe('tag channel')
   })
 
   test('returns null for non-existent channel', async () => {
