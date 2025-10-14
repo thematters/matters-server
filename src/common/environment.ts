3a7cfda47fb386f654688134195bc29be9209b14
Jerry <norepy@github.com>
2025-10-14 11:25:59 -0400

    feat(spam): detect spam after moments/comments creation

--- src/common/environment.ts
+++ src/common/environment.ts
@@ -170,6 +170,8 @@ export const environment = {
   passphrasesSecret: process.env.MATTERS_PASSPHRASES_SECRET || '',
 
   spamDetectionApiUrl: process.env.MATTERS_SPAM_DETECTION_API_URL || '',
+  shortContentSpamDetectionApiUrl:
+    process.env.MATTERS_SHORT_CONTENT_SPAM_DETECTION_API_URL || '',
   channelClassificationApiUrl:
     process.env.MATTERS_CHANNEL_CLASSIFICATION_API_URL || '',
   languageDetectionApiUrl: process.env.MATTERS_LANGUAGE_DETECTION_API_URL || '',
