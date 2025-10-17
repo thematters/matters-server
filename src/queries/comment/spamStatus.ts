01c77335fd078994068aa0e3daa472834b1cd3f8
Jerry <norepy@github.com>
2025-10-17 03:46:12 -0400

    feat(spam): detect spam when spam score is null

--- src/queries/comment/spamStatus.ts
+++ src/queries/comment/spamStatus.ts
@@ -1,8 +1,12 @@
 import type { GQLCommentResolvers } from '#definitions/index.js'
 
-export const spamStatus: GQLCommentResolvers['spamStatus'] = async ({
-  spamScore,
-  isSpam,
-}) => {
+export const spamStatus: GQLCommentResolvers['spamStatus'] = async (
+  { id, content, spamScore, isSpam },
+  _,
+  { dataSources: { commentService } }
+) => {
+  if (!spamScore) {
+    commentService.detectSpam({ id, content: content ?? '' })
+  }
   return { score: spamScore, isSpam }
 }
