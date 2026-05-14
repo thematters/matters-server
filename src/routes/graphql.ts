d51b6878982b29906b938379de2a00257127a4fb
Zeck Li <11781254+zeckli@users.noreply.github.com>
2026-05-14 18:44:12 +0800

    fix(playground): hotfix for production and dev playground disable

--- src/routes/graphql.ts
+++ src/routes/graphql.ts
@@ -244,7 +244,7 @@ export const graphql = async (app: Express) => {
     expressMiddleware<Context>(server, { context: makeContext })
   )
 
-  if (!isProd) {
+  if (isLocal) {
     app.get(
       PLAYGROUND_ENDPOINT,
       expressPlayground({
